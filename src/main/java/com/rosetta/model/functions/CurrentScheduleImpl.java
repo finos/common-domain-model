e914814302614f277298d3812f391a28b471db6c
Dennis Huebner <dennis.huebner@gmail.com>
2018-04-19 16:47:14 +0200

    Changed fefault output #412

---
+++ src/main/java/com/rosetta/model/functions/CurrentScheduleImpl.java
@@ -0,0 +1,10 @@
+package com.rosetta.model.functions;
+
+import  com.rosetta.model.functions.CurrentSchedule.Result;
+
+public class CurrentScheduleImpl {
+	
+	public Result execute(com.rosetta.model.FloatingRateCalculation floatingRateCalculation){
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
---
+++ src/main/java/com/rosetta/model/functions/ResolveRateIndexImpl.java
@@ -0,0 +1,10 @@
+package com.rosetta.model.functions;
+
+import  com.rosetta.model.functions.ResolveRateIndex.Result;
+
+public class ResolveRateIndexImpl {
+	
+	public Result execute(com.rosetta.model.FloatingRateIndexEnum index){
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
