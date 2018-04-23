b1e54c8eb7914890c82f886e2983635331774353
Jim Wang <jim.wang@regnosys.com>
2018-04-23 16:20:27 +0100

    Implement CurrentScheduleImpl

--- src/main/java/com/rosetta/model/functions/CurrentScheduleImpl.java
+++ src/main/java/com/rosetta/model/functions/CurrentScheduleImpl.java
@@ -1,10 +1,25 @@
 package com.rosetta.model.functions;
 
-import  com.rosetta.model.functions.CurrentSchedule.Result;
+import com.rosetta.model.SpreadSchedule;
+import com.rosetta.model.functions.CurrentSchedule.Result;
+
+import java.util.List;
 
 public class CurrentScheduleImpl {
-	
-	public Result execute(com.rosetta.model.FloatingRateCalculation floatingRateCalculation){
-		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+
+	public Result execute(com.rosetta.model.FloatingRateCalculation floatingRateCalculation) {
+        List<SpreadSchedule> spreadSchedules = floatingRateCalculation.getSpreadSchedule();
+
+        if (spreadSchedules != null && !spreadSchedules.isEmpty()) {
+            return new Result().setSchedule(spreadSchedules.get(0));
+        } else {
+            throw new CurrentScheduleImplException("No rate schedule found on leg");
+        }
 	}
+
+	private static class CurrentScheduleImplException extends RuntimeException {
+        private CurrentScheduleImplException(String message) {
+            super(message);
+        }
+    }
 }
