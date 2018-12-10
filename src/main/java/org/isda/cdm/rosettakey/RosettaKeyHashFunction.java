b812e61b3f74feac1a058c04aee9af7aff32ecef
Jim Wang <jim.wang@regnosys.com>
2018-12-10 15:10:33 +0000

    Better javadoc

--- src/main/java/org/isda/cdm/rosettakey/RosettaKeyHashFunction.java
+++ src/main/java/org/isda/cdm/rosettakey/RosettaKeyHashFunction.java
@@ -3,6 +3,10 @@ package org.isda.cdm.rosettakey;
 import com.rosetta.model.lib.HashFunction;
 import com.rosetta.model.lib.RosettaKey;
 
+/**
+ * Combines a {@link com.rosetta.model.lib.HashHelper} and {@link HashFunction} so that we have an easy way of asking a
+ * {@link RosettaKey} model object for its hash value.
+ */
 public class RosettaKeyHashFunction extends IntegerHashHelper implements HashFunction<RosettaKey, Integer> {
 
     @Override
