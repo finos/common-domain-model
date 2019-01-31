52ade93284b3435155a0a69c77aff4f189f2f6cf
Minesh Patel <minesh.patel@regnosys.com>
2019-01-31 10:23:19 +0000

    Implemented interfaces

--- src/main/java/org/isda/cdm/functions/PeriodsInYearImpl.java
+++ src/main/java/org/isda/cdm/functions/PeriodsInYearImpl.java
@@ -1,11 +1,14 @@
 package org.isda.cdm.functions;
 
 import org.isda.cdm.CalculationPeriodDates;
+import org.isda.cdm.CalculationPeriodFrequency;
 import org.isda.cdm.functions.PeriodsInYear.CalculationResult;
 
-public class PeriodsInYearImpl {
-	
-	public CalculationResult execute(CalculationPeriodDates calculationPeriodDates) {
+public class PeriodsInYearImpl implements PeriodsInYear {
+
+	@Override
+	public CalculationResult execute(CalculationPeriodFrequency calculationPeriodDates) {
 		throw new UnsupportedOperationException("TODO: auto-generated method stub");
 	}
+
 }
