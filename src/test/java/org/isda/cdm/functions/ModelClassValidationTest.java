fa25fbc1ac6e5b5ef4bf8d147811f39ca68c6810
TomForwood <Tom.Forwood@regnosys.com>
2019-04-12 14:21:06 +0100

    change to new reference style

--- src/test/java/org/isda/cdm/functions/ModelClassValidationTest.java
+++ src/test/java/org/isda/cdm/functions/ModelClassValidationTest.java
@@ -1,12 +1,12 @@
 package org.isda.cdm.functions;
 
 import com.rosetta.model.lib.validation.ValidationResult;
-import com.rosetta.model.metafields.*;
 
 import org.isda.cdm.Contract;
 import org.isda.cdm.PartyRole;
 import org.isda.cdm.PartyRoleEnum;
 import org.isda.cdm.meta.ContractMeta;
+import org.isda.cdm.metafields.ReferenceWithMetaParty;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,11 +18,11 @@ public class ModelClassValidationTest {
 		Contract contract = 
 				Contract.builder()
 					.addPartyRole(PartyRole.builder()
-							.setPartyReference(ReferenceWithMetaParty.builder().setReference("party1").build())
+							.setPartyReference(ReferenceWithMetaParty.builder().setExternalReference("party1").build())
 							.setRole(PartyRoleEnum.DETERMINING_PARTY)
 							.build())
 					.addPartyRole(PartyRole.builder()
-							.setPartyReference(ReferenceWithMetaParty.builder().setReference("party1").build())
+							.setPartyReference(ReferenceWithMetaParty.builder().setExternalReference("party1").build())
 							.setRole(PartyRoleEnum.DETERMINING_PARTY)
 							.build())
 					.build();
