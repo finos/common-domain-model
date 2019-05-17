f0f70ddf7bb70b094537627c4ae130d38f7171da
Jim Wang <jim.wang@regnosys.com>
2019-05-17 10:35:03 +0100

    Try to resolve references

---
+++ src/main/java/org/isda/cdm/functions/SpotRateImpl.java
@@ -0,0 +1,11 @@
+package org.isda.cdm.functions;
+
+import org.isda.cdm.ForeignExchange;
+import org.isda.cdm.functions.SpotRate.CalculationResult;
+
+public class SpotRateImpl {
+	
+	public CalculationResult execute(ForeignExchange fx) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
---
+++ src/main/java/org/isda/cdm/functions/TodayImpl.java
@@ -0,0 +1,10 @@
+package org.isda.cdm.functions;
+
+import org.isda.cdm.functions.Today.CalculationResult;
+
+public class TodayImpl {
+	
+	public CalculationResult execute() {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
