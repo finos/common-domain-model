5bca76addb699c307828d6403d1946d4945ce062
Jim Wang <jim.wang@regnosys.com>
2019-06-13 21:47:29 +0100

    Syntax update due to grammar change

---
+++ src/main/java/org/isda/cdm/functions/ResolveInitialPriceImpl.java
@@ -0,0 +1,11 @@
+package org.isda.cdm.functions;
+
+import org.isda.cdm.EquityPayout;
+import org.isda.cdm.functions.ResolveInitialPrice.CalculationResult;
+
+public class ResolveInitialPriceImpl {
+	
+	public CalculationResult execute(EquityPayout equityPayout) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
---
+++ src/main/java/org/isda/cdm/functions/ResolveRateOfReturnImpl.java
@@ -0,0 +1,11 @@
+package org.isda.cdm.functions;
+
+import org.isda.cdm.EquityPayout;
+import org.isda.cdm.functions.ResolveRateOfReturn.CalculationResult;
+
+public class ResolveRateOfReturnImpl {
+	
+	public CalculationResult execute(EquityPayout equityPayout) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
