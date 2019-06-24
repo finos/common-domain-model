1e43a121c804085d7fe1641d97436f3ee097d45d
Jim Wang <jim.wang@regnosys.com>
2019-06-24 23:45:53 +0100

    Function spec codegen (#432)
    * Turn on code gen for equity specs
    * Turn on code gen for Quantity Change primitive

---
+++ src/main/java/org/isda/cdm/functions/ResolveCalculationPeriodDatesImpl.java
@@ -0,0 +1,11 @@
+package org.isda.cdm.functions;
+
+import org.isda.cdm.CalculationPeriodDates;
+import org.isda.cdm.functions.ResolveCalculationPeriodDates.CalculationResult;
+
+public class ResolveCalculationPeriodDatesImpl {
+	
+	public CalculationResult execute(CalculationPeriodDates calculationPeriodDates) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
