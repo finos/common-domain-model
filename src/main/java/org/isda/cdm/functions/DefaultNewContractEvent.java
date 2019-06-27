1f0bca5582e5f4f15a94dcf175f06ff71c6bd927
Jim Wang <jim.wang@regnosys.com>
2019-06-27 22:05:23 +0100

    Stamp identifiers needed for lineage

--- src/main/java/org/isda/cdm/functions/DefaultNewContractEvent.java
+++ src/main/java/org/isda/cdm/functions/DefaultNewContractEvent.java
@@ -3,6 +3,8 @@ package org.isda.cdm.functions;
 import com.google.common.collect.ClassToInstanceMap;
 import com.rosetta.model.lib.functions.RosettaFunction;
 import org.isda.cdm.*;
+import org.isda.cdm.metafields.FieldWithMetaString;
+import org.isda.cdm.metafields.ReferenceWithMetaParty;
 
 public class DefaultNewContractEvent extends NewContractEvent {
 
@@ -13,6 +15,17 @@ public class DefaultNewContractEvent extends NewContractEvent {
     @Override
     Event doEvaluate(Product product, Party partyA, Party partyB, LegalAgreement legalAgreement) {
         return Event.builder()
+                .addEventIdentifierBuilder(Identifier.builder()
+                    .addAssignedIdentifierBuilder(AssignedIdentifier.builder()
+                        .setIdentifier(FieldWithMetaString.builder()  // Implementors will likely use an Identifier Service to produce this value
+                                .setValue(partyA.getMeta().getGlobalKey())
+                                .build())
+                        .setVersion(1))
+                    .setIssuer(FieldWithMetaString.builder()
+                            .setValue(partyA.getMeta().getGlobalKey())
+                            .build())
+                    .setIssuerReferenceBuilder(ReferenceWithMetaParty.builder()
+                        .setExternalReference(partyA.getMeta().getExternalKey())))
                 .addParty(partyA)
                 .addParty(partyB)
                 .setPrimitiveBuilder(PrimitiveEvent.builder()
