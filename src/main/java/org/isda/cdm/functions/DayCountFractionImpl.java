694487c9c16a4263e683ddbd90d484d31792577d
jim-h-wang <jim.wang@regnosys.com>
2019-09-02 22:32:37 +0100

    migrate remaining functions and spec

---
+++ src/main/java/org/isda/cdm/functions/DayCountFractionImpl.java
@@ -0,0 +1,13 @@
+package org.isda.cdm.functions;
+
+import java.math.BigDecimal;
+import org.isda.cdm.DayCountFractionEnum;
+import org.isda.cdm.InterestRatePayout;
+
+public class DayCountFractionImpl extends DayCountFraction {
+	
+	@Override
+	protected BigDecimal doEvaluate(InterestRatePayout interestRatePayout, DayCountFractionEnum dayCountFractionEnum) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub");
+	}
+}
