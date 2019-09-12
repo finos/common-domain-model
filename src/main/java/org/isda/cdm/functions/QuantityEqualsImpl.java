e0761dbd1e4daf7de00734195b942485e52e9cd3
Dennis Huebner <dennis.huebner@gmail.com>
2019-09-12 14:12:38 +0200

    Make use of 'empty' and 'only-element' (#489)

--- src/main/java/org/isda/cdm/functions/QuantityEqualsImpl.java
+++ src/main/java/org/isda/cdm/functions/QuantityEqualsImpl.java
@@ -1,14 +1,13 @@
 package org.isda.cdm.functions;
 
-import java.lang.Boolean;
 import java.math.BigDecimal;
-import java.util.List;
+
 import org.isda.cdm.Quantity;
 
 public class QuantityEqualsImpl extends QuantityEquals {
-	
+
 	@Override
-	protected Boolean doEvaluate(List<Quantity> q1, Quantity q2, BigDecimal precision) {
+	protected Boolean doEvaluate(Quantity q1, Quantity q2, BigDecimal precision) {
 		throw new UnsupportedOperationException("TODO: auto-generated method stub");
 	}
 }
---
+++ src/main/java/org/isda/cdm/functions/SumImpl.java
@@ -0,0 +1,14 @@
+package org.isda.cdm.functions;
+
+import java.util.List;
+
+import org.isda.cdm.Quantity;
+
+public class SumImpl extends Sum {
+
+	@Override
+	protected Quantity doEvaluate(List<Quantity> quantities) {
+		return null;
+	}
+
+}
