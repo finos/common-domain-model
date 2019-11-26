d004fa9f0b6bef56a3baa5a5b5011d4995b84091
Minesh Patel <minesh.patel@regnosys.com>
2019-11-26 17:18:43 +0000

    Fix the fx mark to market function and tests

--- src/test/java/org/isda/cdm/functions/AbstractFunctionTest.java
+++ src/test/java/org/isda/cdm/functions/AbstractFunctionTest.java
@@ -1,27 +1,35 @@
 package org.isda.cdm.functions;
 
+import com.google.inject.Module;
+import com.google.inject.*;
+import com.google.inject.util.Modules;
 import org.isda.cdm.CdmTestsModule;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 public abstract class AbstractFunctionTest {
 	private static Injector injector;
 
-	@BeforeAll
-	public static void setUpOnce() {
-		injector = Guice.createInjector(new CdmTestsModule());
-	}
-
 	@BeforeEach
 	public void setUp() {
+		injector = Guice.createInjector(testingModule());
 		injector.injectMembers(this);
 	}
-	
+
+	private Module testingModule() {
+		AbstractModule module = new AbstractModule() {
+			@Override protected void configure() {
+				bindTestingMocks(binder());
+			}
+		};
+		return Modules.override(new CdmTestsModule()).with(module);
+	}
+
+	protected void bindTestingMocks(Binder binder) {
+	}
+
 	/**
 	 * Don't use Injector directly! Use @Inject annotation to inject functions
+	 *
 	 * @return
 	 */
 	@Deprecated
