8cc927a5dbafeec87bceee7dc7d70387b5f1c814
David Al-Kanani <david.al-kanani@regnosys.com>
2020-01-21 11:43:18 +0000

    fix size check

--- src/main/java/org/isda/cdm/functions/PartyByRoleImpl.java
+++ src/main/java/org/isda/cdm/functions/PartyByRoleImpl.java
@@ -1,7 +1,7 @@
 package org.isda.cdm.functions;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import org.isda.cdm.Party.PartyBuilder;
 import org.isda.cdm.Party;
@@ -12,13 +12,14 @@ public class PartyByRoleImpl extends PartyByRole {
 
 	@Override
 	protected PartyBuilder doEvaluate(List<PartyRole> roles, PartyRoleEnum role) {
-		Stream<PartyRole> partyRoleStream = roles.stream().filter(r -> r.getRole().equals(role));
-		if (partyRoleStream.count() > 1) throw new RuntimeException("Multiple roles of the same type found");
 		
-		return partyRoleStream
+		List<PartyBuilder> partyBuilders = roles.stream().filter(r -> r.getRole().equals(role))
 				.map(partyRole -> partyRole.getPartyReference().getValue().toBuilder())
-				.findFirst().orElse(Party.builder());
+				.collect(Collectors.toList());
 		
+		if (partyBuilders.size() > 1) throw new RuntimeException("Multiple roles of the same type found");
+		
+		return partyBuilders.size() == 0 ? Party.builder() : partyBuilders.get(0);
 	}
 
 }
