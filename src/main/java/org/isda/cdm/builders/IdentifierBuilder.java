a0643dda0407d5eef72ac79274151810e816a462
fahd <fahd.akbar@regnosys.com>
2020-01-24 14:21:44 +0000

    java changes

--- src/main/java/org/isda/cdm/builders/IdentifierBuilder.java
+++ src/main/java/org/isda/cdm/builders/IdentifierBuilder.java
@@ -1,15 +1,15 @@
 package org.isda.cdm.builders;
 
-import org.isda.cdm.metafields.FieldWithMetaString;
-import org.isda.cdm.metafields.ReferenceWithMetaParty;
-
 import cdm.base.staticdata.identifier.AssignedIdentifier;
 import cdm.base.staticdata.identifier.Identifier;
+import cdm.base.staticdata.identifier.metafields.FieldWithMetaString;
+import cdm.base.staticdata.party.metafields.ReferenceWithMetaParty;
 
 public class IdentifierBuilder {
 
     public static Identifier.IdentifierBuilder get(String issuer, String id, int version) {
         return Identifier.builder()
+        		
                 .setIssuerReferenceBuilder(ReferenceWithMetaParty.builder()
                         .setExternalReference(issuer))
                 .addAssignedIdentifierBuilder(AssignedIdentifier.builder()
