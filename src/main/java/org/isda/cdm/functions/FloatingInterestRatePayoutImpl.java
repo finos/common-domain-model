cc519b5053a021c9cd9c74721e52e578ceeaf81a
Minesh Patel <minesh.patel@regnosys.com>
2020-03-06 09:22:17 +0000

    Fix NPE

--- src/main/java/org/isda/cdm/functions/FloatingInterestRatePayoutImpl.java
+++ src/main/java/org/isda/cdm/functions/FloatingInterestRatePayoutImpl.java
@@ -1,20 +1,29 @@
 package org.isda.cdm.functions;
 
+import org.isda.cdm.InterestRatePayout;
+import org.isda.cdm.InterestRatePayout.InterestRatePayoutBuilder;
+import org.isda.cdm.RateSpecification;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.isda.cdm.InterestRatePayout;
-import org.isda.cdm.InterestRatePayout.InterestRatePayoutBuilder;
-
 public class FloatingInterestRatePayoutImpl extends FloatingInterestRatePayout {
 
-	@Override
-	protected InterestRatePayoutBuilder doEvaluate(List<InterestRatePayout> interestRatePayouts) {
-		Optional<InterestRatePayoutBuilder> floating = interestRatePayouts.stream()
-			.filter(x -> x.getRateSpecification().getFixedRate() != null)
-			.findFirst()
-			.map(x -> x.toBuilder());	
-		return floating.orElse(null);
-	}
+    @Override
+    protected InterestRatePayoutBuilder doEvaluate(List<InterestRatePayout> interestRatePayouts) {
+        Optional<InterestRatePayoutBuilder> floating = interestRatePayouts.stream()
+                .filter(this::isFixedRate)
+                .findFirst()
+                .map(InterestRatePayout::toBuilder);
+        return floating.orElse(null);
+    }
 
+    private boolean isFixedRate(InterestRatePayout interestRatePayout) {
+        return Optional.ofNullable(interestRatePayout)
+                .map(InterestRatePayout::toBuilder)
+                .map(InterestRatePayoutBuilder::prune)
+                .map(InterestRatePayoutBuilder::getRateSpecification)
+                .map(RateSpecification.RateSpecificationBuilder::getFixedRate)
+                .isPresent();
+    }
 }
