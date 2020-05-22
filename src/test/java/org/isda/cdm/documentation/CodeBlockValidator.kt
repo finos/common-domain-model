56779d737890fe81469c0d2b18288f16b3f341e2
jim-h-wang <jim.wang@regnosys.com>
2020-05-22 22:46:51 +0100

    Pass in file-paths
    Remove invalid snippet syntax in-place
    
    delete test doc file

--- src/test/java/org/isda/cdm/documentation/CodeBlockValidator.kt
+++ src/test/java/org/isda/cdm/documentation/CodeBlockValidator.kt
@@ -1,5 +1,7 @@
 package org.isda.cdm.documentation
 
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Options
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Path
@@ -8,19 +10,16 @@ import java.nio.file.attribute.BasicFileAttributes
 import java.util.function.BiPredicate
 import java.util.stream.Collectors
 
-class CodeBlockValidator {
-
-    private val docFile = "/documentation-test.txt"
-    private val modelDir = "cdm/rosetta"
-
-    private val rosettaFileFilter = BiPredicate<Path, BasicFileAttributes> { path, attr -> attr.isRegularFile && path.fileName.toString().endsWith(".rosetta") }
+class CodeBlockValidator(
+        private val docPath: String,
+        private val modelPath: String,
+        private val fixUp: Boolean) {
 
     private val synonymRegex = "\\[synonym [^\\]]*\\]"
     private val definitionRegex = "<\".*\">"
-    private val lineCommentRegex = "\\/\\/.*"
+    private val lineCommentRegex = "[^:]\\/\\/.*\$"
     private val whitespaceRegex = "\\s+"
     private val illegalSyntaxRegex = "$synonymRegex|$definitionRegex|$lineCommentRegex"
-
     private val codeBlockRegex = Regex("(\\.\\. code-block:: .*\\s+$)((\\n +.*|\\s)+)", RegexOption.MULTILINE)
 
     fun validate() {
@@ -29,35 +28,44 @@ class CodeBlockValidator {
         val invalidBlocks = codeBlocks
                 .filter { codeBlock -> !model.contains(codeBlock) }
                 .onEach { println(it) }
-                .toSet()
 
-        if (invalidBlocks.isNotEmpty()) {
-            throw java.lang.IllegalStateException("Found code blocks that don't match model text")
+        val blocks = invalidBlocks.count()
+        if (blocks > 0) {
+            throw java.lang.IllegalStateException("Found [$blocks] code blocks that don't match model text")
         }
     }
 
     private fun getCodeBlocks(): Sequence<String> {
-        val doc = this::class.java.getResource(docFile).readText()
+        val doc = Path.of(docPath).toUri().toURL().readText()
 
         val codeBlocks = codeBlockRegex.findAll(doc)
                 .map(MatchResult::value)
-                .ifEmpty { throw IllegalStateException("No code blocks found in documentation file [$docFile]. Doesn't sound right! Go check.") }
+                .ifEmpty { throw IllegalStateException("No code blocks found in documentation file [$docPath]. Doesn't sound right! Go check.") }
 
         val illegalCodeBlocks = codeBlocks
                 .filter { it.contains(Regex(illegalSyntaxRegex, RegexOption.MULTILINE)) }
                 .onEach(::println)
 
-        if (illegalCodeBlocks.count() > 0) {
-            throw java.lang.IllegalStateException("Found illegal code syntax in documentation file [$docFile]. Line comments, definitions, and synonyms are not allowed in code snippets")
+        val illegalBlocks = illegalCodeBlocks.count()
+
+        return if (illegalBlocks > 0) {
+            if (fixUp) {
+                println("WARNING in fix-up mode, removing illegal syntax from [$docPath]")
+                val sanitised = doc.replace(Regex(illegalSyntaxRegex, RegexOption.MULTILINE), "")
+                Files.writeString(Paths.get(docPath), sanitised)
+                getCodeBlocks()
+            } else {
+                throw java.lang.IllegalStateException("Found [$illegalBlocks] invalid code blocks in documentation file [$docPath]. Line comments, definitions, and synonyms are not allowed in code snippets")
+            }
         } else {
-            return codeBlocks
+            codeBlocks
         }
     }
 
     private fun getModel(): String {
-        val rosettaFileFilter = rosettaFileFilter
-        val modelDirectory = this::class.java.classLoader.getResource(modelDir) ?: throw IllegalStateException("No model code found in classpath: $modelDir")
-        return Files.find(Paths.get(modelDirectory.toURI()), 1, rosettaFileFilter)
+        val rosettaFileFilter = BiPredicate<Path, BasicFileAttributes> { path, attr -> attr.isRegularFile && path.fileName.toString().endsWith(".rosetta") }
+
+        return Files.find(Path.of(modelPath), 1, rosettaFileFilter)
                 .map { path -> Files.readString(path, Charset.defaultCharset()) }.collect(Collectors.joining())
                 .replace(Regex(illegalSyntaxRegex, RegexOption.MULTILINE), "")
                 .replace(Regex(whitespaceRegex), "")
@@ -65,5 +73,16 @@ class CodeBlockValidator {
 }
 
 fun main(args: Array<String>) {
-    CodeBlockValidator().validate()
+    val options = Options()
+            .addOption("u", "fix-up", false, "Remove illegal code syntax from document file in-place.")
+            .addOption("d", "doc-path", true, "Relative path to the document.")
+            .addOption("m", "model-path", true, "Relative path to the model files directory.")
+
+    val cmd = DefaultParser().parse(options, args)
+
+    val docPath = cmd.getOptionValue("doc-path") ?: "documentation/source/documentation.rst"
+    val modelPath = cmd.getOptionValue("model-path") ?: "src/main/rosetta"
+    val fixUp = cmd.hasOption("fix-up")
+
+    CodeBlockValidator(docPath, modelPath, fixUp).validate()
 }
