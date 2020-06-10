ca92b0ce9f6e2d9df612aeee35f6b4ec0088d927
Hugo Hills <hugo.hills@regnosys.com>
2020-06-10 16:36:15 +0100

    Add CollateralManagementAgreement mapper

---
+++ src/main/java/org/isda/cdm/processor/CollateralManagementAgreementMappingProcessor.java
@@ -0,0 +1,38 @@
+package org.isda.cdm.processor;
+
+import com.regnosys.rosetta.common.translation.Mapping;
+import com.rosetta.model.lib.RosettaModelObjectBuilder;
+import com.rosetta.model.lib.path.RosettaPath;
+import org.isda.cdm.CollateralManagementAgreement;
+import org.isda.cdm.CollateralManagementAgreementElection;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.isda.cdm.processor.RegimeMappingHelper.PARTIES;
+
+public class CollateralManagementAgreementMappingProcessor extends MappingProcessor {
+
+	public CollateralManagementAgreementMappingProcessor(RosettaPath rosettaPath, List<String> synonymValues, List<Mapping> mappings) {
+		super(rosettaPath, synonymValues, mappings);
+	}
+
+	@Override
+	public void map(RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent) {
+		CollateralManagementAgreement.CollateralManagementAgreementBuilder collateralManagementAgreement =
+				(CollateralManagementAgreement.CollateralManagementAgreementBuilder) builder;
+		PARTIES.forEach(party -> getCollateralManagementAgreementElection(party).ifPresent(collateralManagementAgreement::addPartyElection));
+	}
+
+	private Optional<CollateralManagementAgreementElection> getCollateralManagementAgreementElection(String party) {
+		CollateralManagementAgreementElection.CollateralManagementAgreementElectionBuilder electionBuilder = CollateralManagementAgreementElection.builder();
+
+		setValueAndUpdateMappings(String.format("answers.partyA.collateral_management_agreement.%s_specify", party),
+				(value) -> {
+					electionBuilder.setParty(party);
+					electionBuilder.setCollateralManagementAgreement(value);
+				});
+
+		return electionBuilder.hasData() ? Optional.of(electionBuilder.build()) : Optional.empty();
+	}
+}
