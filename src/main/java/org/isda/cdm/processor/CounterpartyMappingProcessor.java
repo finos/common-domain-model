9e56ed425d2a41920668d8e67c42cce6f4978e26
Hugo Hills <hugo.hills@regnosys.com>
2020-06-24 10:17:06 +0100

    Work in progress

--- src/main/java/org/isda/cdm/processor/CounterpartyMappingProcessor.java
+++ src/main/java/org/isda/cdm/processor/CounterpartyMappingProcessor.java
@@ -1,64 +1,73 @@
 package org.isda.cdm.processor;
 
-import com.regnosys.rosetta.common.translation.Mapping;
+import cdm.base.staticdata.party.CounterpartyEnum;
+import com.regnosys.rosetta.common.translation.MappingContext;
+import com.regnosys.rosetta.common.translation.MappingProcessor;
 import com.regnosys.rosetta.common.translation.Path;
-import com.rosetta.model.lib.RosettaModelObject;
 import com.rosetta.model.lib.RosettaModelObjectBuilder;
 import com.rosetta.model.lib.path.RosettaPath;
-import com.rosetta.model.lib.process.AttributeMeta;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.setValueAndOptionallyUpdateMappings;
+import static com.regnosys.rosetta.common.util.StringExtensions.toFirstUpper;
 
 public class CounterpartyMappingProcessor extends MappingProcessor {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CounterpartyMappingProcessor.class);
+	private static final String COUNTERPARTY_MAP = "counterpartyMap";
+	private final Map<String, CounterpartyEnum> externalReferenceToCounterpartyMap;
 
-	public CounterpartyMappingProcessor(RosettaPath rosettaPath, List<String> synonymValues, List<Mapping> mappings) {
-		super(rosettaPath, synonymValues, mappings);
-	}
-
-	@Override public <R extends RosettaModelObject> boolean processRosetta(RosettaPath currentPath, Class<? extends R> rosettaType,
-			RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent, AttributeMeta... meta) {
-		return super.processRosetta(currentPath, rosettaType, builder, parent, meta);
-	}
-
-	@Override public <R extends RosettaModelObject> boolean processRosetta(RosettaPath currentPath, Class<? extends R> rosettaType,
-			List<? extends RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent, AttributeMeta... meta) {
-		return super.processRosetta(currentPath, rosettaType, builder, parent, meta);
-	}
-
-	@Override public <T> void processBasic(RosettaPath currentPath, Class<T> rosettaType, T instance, RosettaModelObjectBuilder parent, AttributeMeta... meta) {
-//		if (RosettaPath.valueOf("Contract.tradableProduct.product.contractualProduct.economicTerms.payout.interestRatePayout(1).payerReceiver.payer").matchesIgnoringIndex(currentPath)) {
-//			System.out.println("blah");
-//		}
-		super.processBasic(currentPath, rosettaType, instance, parent, meta);
-
-		List<Path> xmlPathsToHere = null;
-		List<String> synonymValues = xmlPathsToHere.stream()
-				.map(xmlPath -> Stream.of("top.leg")
-						.map(Path::parse)
-						.map(xmlPath::append)
-						.map(Path::toString))
-				.flatMap(i -> i)
-				.collect(Collectors.toList());
-	}
-
-	@Override public <T> void processBasic(RosettaPath currentPath, Class<T> rosettaType, List<T> instance, RosettaModelObjectBuilder parent,
-			AttributeMeta... meta) {
-		super.processBasic(currentPath, rosettaType, instance, parent, meta);
+	public CounterpartyMappingProcessor(RosettaPath rosettaPath, List<Path> synonymPaths, MappingContext mappingContext) {
+		super(rosettaPath, synonymPaths, mappingContext);
+		this.externalReferenceToCounterpartyMap = (Map<String, CounterpartyEnum>)
+				getParams().computeIfAbsent(COUNTERPARTY_MAP, (key) -> new HashMap<String, CounterpartyEnum>());
 	}
 
 	@Override
-	protected <R extends RosettaModelObject> void map(RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent) {
-		LOGGER.info("****1 {} {}", builder, parent);
+	protected <T> void mapBasic(Path synonymPath, Optional<T> instance, RosettaModelObjectBuilder parent) {
+		//		Optional<String> blah = getNonNullMappedValue(filterMappings(getMappings(), synonymPath));
+		//		LOGGER.info("**** {} {} {}", synonymPath, blah, PayerReceiver.PayerReceiverBuilder.class.isInstance(parent));
+		//		addToList();
+		setValueAndOptionallyUpdateMappings(
+				synonymPath,
+				(value) -> {
+					Optional<CounterpartyEnum> counterpartyEnum = getCounterpartyEnum(value);
+					counterpartyEnum.ifPresent(c -> invokeSetter(parent, getModelPath().getElement().getPath(), c));
+					return counterpartyEnum.isPresent();
+				},
+				getMappings(),
+				getModelPath());
 	}
 
-	@Override
-	protected <T> void mapBasic(T value, RosettaModelObjectBuilder parent) {
-		LOGGER.info("****2 {} {}", value, parent);
+	private void invokeSetter(RosettaModelObjectBuilder parent, String attribute, CounterpartyEnum counterparty) {
+		try {
+			parent.getClass()
+					.getMethod("set" + toFirstUpper(attribute), CounterpartyEnum.class)
+					.invoke(parent, counterparty);
+		} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Optional<CounterpartyEnum> getCounterpartyEnum(String externalReference) {
+		return Optional.ofNullable(externalReferenceToCounterpartyMap.computeIfAbsent(externalReference,
+				(key) -> {
+					if (externalReferenceToCounterpartyMap.isEmpty()) {
+						return CounterpartyEnum.PARTY_1;
+					} else if (externalReferenceToCounterpartyMap.size() == 1) {
+						return CounterpartyEnum.PARTY_2;
+					} else {
+						return null;
+					}
+				}));
+	}
+
+	private void addToList() {
+		List<RosettaPath> counterparty = (List<RosettaPath>) getParams().computeIfAbsent("counterparty", (key) -> new ArrayList<RosettaPath>());
+		counterparty.add(getModelPath());
 	}
 }
