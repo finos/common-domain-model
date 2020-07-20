533721657f418f738de8bfa7145863d6f688afe0
fahd <fahd.akbar@regnosys.com>
2020-07-20 16:51:38 +0100

    V3 CME and FpML changes

--- src/main/java/org/isda/cdm/functions/CdmToStrataMapper.java
+++ src/main/java/org/isda/cdm/functions/CdmToStrataMapper.java
@@ -1,13 +1,8 @@
 package org.isda.cdm.functions;
 
-import java.util.Arrays;
-
-import org.isda.cdm.CalculationPeriodDates;
-import org.isda.cdm.RollConventionEnum;
-
 import com.opengamma.strata.basics.schedule.Frequency;
 import com.opengamma.strata.basics.schedule.RollConvention;
-import com.rosetta.model.lib.annotations.RosettaSynonym;
+import org.isda.cdm.CalculationPeriodDates;
 
 class CdmToStrataMapper {
 
@@ -15,30 +10,13 @@ class CdmToStrataMapper {
         return Frequency.parse(calculationPeriodDates.getCalculationPeriodFrequency().getPeriodMultiplier().toString() + calculationPeriodDates.getCalculationPeriodFrequency().getPeriod().toString());
     }
 
+    //
     static RollConvention getRollConvention(CalculationPeriodDates calculationPeriodDates) {
-        try {
-            String field = calculationPeriodDates.getCalculationPeriodFrequency().getRollConvention().name();
-            RosettaSynonym[] synonyms = RollConventionEnum.class.getField(field).getAnnotationsByType(RosettaSynonym.class);
-            String rollConventionName = Arrays.stream(synonyms)
-                    .filter(synonym -> synonym.source().equals("FpML_5_10"))
-                    .findFirst()
-                    .map(RosettaSynonym::value)
-                    .orElseThrow(() -> new CdmToStrataMapperException("No FpML synonym found for [" + field + "]"));
-
+            String rollConventionName = calculationPeriodDates.getCalculationPeriodFrequency().getRollConvention().toString();
+            // The display name of the match RollConvention using FpML
             return RollConvention.extendedEnum().externalNames("FpML").lookup(rollConventionName);
 
-        } catch (NoSuchFieldException e) {
-            throw new CdmToStrataMapperException(e);
-        }
+
     }
 
-    private static class CdmToStrataMapperException extends RuntimeException {
-        CdmToStrataMapperException(Throwable cause) {
-            super(cause);
-        }
-
-        CdmToStrataMapperException(String message) {
-            super(message);
-        }
-    }
 }
