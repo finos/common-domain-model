9f95e05bf4db778a3e068b48db807c4808fbfdc4
tsanborn_auth0_5e5599708f02500d6a31cd18 <tsanborn@isda.org>
2020-08-03 15:19:54 +0000

    Per the specification approved by the CDM ARC

--- src/main/java/org/isda/cdm/processor/CounterpartyMappingHelper.java
+++ src/main/java/org/isda/cdm/processor/CounterpartyMappingHelper.java
@@ -117,17 +117,15 @@ public class CounterpartyMappingHelper {
 	void addCounterparties(TradableProductBuilder tradableProductBuilder) {
 		bothCounterpartiesCollected
 				.thenRun(() -> CompletableFuture.supplyAsync(() -> tradableProductBuilder)
-						.thenAccept(builder -> {
-							LOGGER.info("Setting TradableProduct.counterparties");
-							builder.clearCounterparties()
-									.addCounterparties(partyExternalReferenceToCounterpartyEnumMap.entrySet().stream()
-											.map(extRefCounterpartyEntry -> Counterparty.builder()
-													.setCounterparty(extRefCounterpartyEntry.getValue())
-													.setPartyBuilder(ReferenceWithMetaParty.builder()
-															.setExternalReference(extRefCounterpartyEntry.getKey()))
-													.build())
-											.collect(Collectors.toList()));
-						}));
+						.thenAccept(builder ->
+								builder.clearCounterparties()
+										.addCounterparties(partyExternalReferenceToCounterpartyEnumMap.entrySet().stream()
+												.map(extRefCounterpartyEntry -> Counterparty.builder()
+														.setCounterparty(extRefCounterpartyEntry.getValue())
+														.setPartyBuilder(ReferenceWithMetaParty.builder()
+																.setExternalReference(extRefCounterpartyEntry.getKey()))
+														.build())
+												.collect(Collectors.toList()))));
 	}
 
 	/**
