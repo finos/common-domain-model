318dca413473dd98c0bbe4b48c7d467dd4787ccc
Hugo Hills <hugo.hills@regnosys.com>
2020-08-26 22:55:51 +0100

    Merge code generation, work in progress

---
+++ src/test/java/org/isda/cdm/merging/MergeTest.java
@@ -0,0 +1,78 @@
+package org.isda.cdm.merging;
+
+import com.google.common.io.Resources;
+import com.regnosys.rosetta.common.merging.SimpleBuilderMerger;
+import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
+import com.regnosys.rosetta.common.util.PathCollectorBuilderProcessor;
+import com.rosetta.model.lib.path.RosettaPath;
+import org.isda.cdm.Product;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import static org.isda.cdm.Product.ProductBuilder;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MergeTest.class);
+
+	private static final String INPUT_1 = "merging/input1.json";
+	private static final String INPUT_2 = "merging/input2.json";
+	private static final String INPUT_3 = "merging/input3.json";
+	private static final String EXPECTED_RESULT = "merging/expected-result.json";
+
+	@Test
+	void shouldMergeInputObjects() throws IOException {
+		ProductBuilder input1 = getProduct(INPUT_1);
+		ProductBuilder input2 = getProduct(INPUT_2);
+
+		ProductBuilder merged = Product.builder()
+				.merge(input1, input2, new SimpleBuilderMerger());
+
+		assertEquals(getProduct(EXPECTED_RESULT), merged);
+	}
+
+	@Test
+	void shouldMergeInputObjectsReverse() throws IOException {
+		ProductBuilder input1 = getProduct(INPUT_1);
+		ProductBuilder input2 = getProduct(INPUT_2);
+
+		ProductBuilder merged = Product.builder()
+				.merge(input2, input1, new SimpleBuilderMerger());
+
+		assertEquals(getProduct(EXPECTED_RESULT), merged);
+	}
+
+	@Test
+	void shouldMergeInputObjectsClash() throws IOException {
+		ProductBuilder input1 = getProduct(INPUT_1);
+		ProductBuilder input3 = getProduct(INPUT_3);
+
+		IllegalArgumentException thrown = assertThrows(
+				IllegalArgumentException.class,
+				() ->Product.builder().merge(input1, input3, new SimpleBuilderMerger()));
+
+		assertEquals("Attempting to merge 2 different basic values [left=INITIAL_BUYER, right=EITHER, type=CallingPartyEnum]", thrown.getMessage());
+	}
+
+	private ProductBuilder getProduct(String resourceName) throws IOException {
+		URL url = Resources.getResource(resourceName);
+		String json = Resources.toString(url, Charset.defaultCharset());
+		return RosettaObjectMapper.getNewRosettaObjectMapper().readValue(json, Product.class).toBuilder();
+	}
+
+	@SuppressWarnings("unused")
+	private void printPathsAndValues(Product product) {
+		PathCollectorBuilderProcessor processor = new PathCollectorBuilderProcessor();
+		ProductBuilder builder = product.toBuilder();
+		builder.process(RosettaPath.valueOf(product.getClass().getSimpleName()), processor);
+		processor.report()
+				.getCollectedPaths()
+				.forEach((key, value) -> LOGGER.debug("{} {}", key.buildPath(), value));
+	}
+}
