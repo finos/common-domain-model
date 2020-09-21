45ab967f24c2475cbdef47b747d3b7f82f93389c
Hugo Hills <hugo.hills@regnosys.com>
2020-09-21 10:53:05 +0100

    Add more unit tests

--- src/test/java/org/isda/cdm/processor/PartyMappingHelperTest.java
+++ src/test/java/org/isda/cdm/processor/PartyMappingHelperTest.java
@@ -1,7 +1,10 @@
 package org.isda.cdm.processor;
 
+import cdm.base.staticdata.party.Counterparty;
 import cdm.base.staticdata.party.CounterpartyEnum;
 import cdm.base.staticdata.party.PayerReceiver;
+import cdm.base.staticdata.party.metafields.ReferenceWithMetaParty;
+import cdm.product.template.TradableProduct;
 import com.regnosys.rosetta.common.translation.Mapping;
 import com.regnosys.rosetta.common.translation.MappingContext;
 import com.regnosys.rosetta.common.translation.Path;
@@ -15,10 +18,17 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static cdm.base.staticdata.party.PayerReceiver.PayerReceiverBuilder;
+import static cdm.product.template.TradableProduct.TradableProductBuilder;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class PartyMappingHelperTest {
 
@@ -89,12 +99,53 @@ class PartyMappingHelperTest {
 		Path payerSynonymPath = PAYER_XML_PATH.getParent();
 		helper.setCounterpartyEnum(builder, PAYER_MODEL_PATH, payerSynonymPath, null);
 
+		assertFalse(helper.getBothCounterpartiesCollectedFuture().isDone());
+
 		Path receiverSynonymPath = RECEIVER_XML_PATH.getParent();
 		helper.setCounterpartyEnum(builder, RECEIVER_MODEL_PATH, receiverSynonymPath, null);
 
 		assertEquals(CounterpartyEnum.PARTY_1, builder.getPayer());
 		assertEquals(CounterpartyEnum.PARTY_2, builder.getReceiver());
 
+		assertTrue(helper.getBothCounterpartiesCollectedFuture().isDone());
+		Map<String, CounterpartyEnum> partyExternalReferenceToCounterpartyEnumMap = helper.getBothCounterpartiesCollectedFuture().get();
+		assertNotNull(partyExternalReferenceToCounterpartyEnumMap);
+		assertEquals(CounterpartyEnum.PARTY_1, partyExternalReferenceToCounterpartyEnumMap.get(PAYER_PARTY_REF));
+		assertEquals(CounterpartyEnum.PARTY_2, partyExternalReferenceToCounterpartyEnumMap.get(RECEIVER_PARTY_REF));
+
+		// test PartyMappingHelper.addCounterparties()
+		TradableProductBuilder tradableProductBuilderMock = mock(TradableProductBuilder.class);
+		helper.supplyTradableProductBuilder(tradableProductBuilderMock);
+
+		// mock tradableProductBuilder.clearCounterparties
+		when(tradableProductBuilderMock.clearCounterparties()).thenReturn(tradableProductBuilderMock);
+
+		// mock and assert tradableProductBuilder.addCounterparties - use countdown latch because this method uses an executor
+		final CountDownLatch latch = new CountDownLatch(1);
+		doAnswer(invocation -> {
+			List<Counterparty> counterparties = invocation.getArgument(0);
+			// assert counterparties
+			assertThat(counterparties, hasSize(2));
+			assertThat(counterparties, hasItem(Counterparty.builder()
+					.setCounterparty(CounterpartyEnum.PARTY_1)
+					.setPartyReference(ReferenceWithMetaParty.builder().setExternalReference(PAYER_PARTY_REF).build())
+					.build()));
+			assertThat(counterparties, hasItem(Counterparty.builder()
+					.setCounterparty(CounterpartyEnum.PARTY_2)
+					.setPartyReference(ReferenceWithMetaParty.builder().setExternalReference(RECEIVER_PARTY_REF).build())
+					.build()));
+			// countdown to proceed with test
+			latch.countDown();
+			return TradableProduct.builder().addCounterparties(counterparties);
+		}).when(tradableProductBuilderMock).addCounterparties(anyList());
+
+		// test
+		helper.addCounterparties();
+
+		// wait for countdown latch as addCounterparties() uses an executor
+		assertTrue(latch.await(60L, TimeUnit.SECONDS));
+
+		// assert mappings
 		Mapping payerUpdatedMapping = context.getMappings().get(0);
 		assertEquals(PAYER_XML_PATH, payerUpdatedMapping.getXmlPath());
 		assertEquals(PAYER_PARTY_REF, payerUpdatedMapping.getXmlValue());
@@ -110,12 +161,6 @@ class PartyMappingHelperTest {
 		assertNull(receiverUpdatedMapping.getRosettaValue());
 		assertNull(receiverUpdatedMapping.getError());
 		assertTrue(receiverUpdatedMapping.isCondition());
-
-		assertTrue(helper.getBothCounterpartiesCollectedFuture().isDone());
-		Map<String, CounterpartyEnum> partyExternalReferenceToCounterpartyEnumMap = helper.getBothCounterpartiesCollectedFuture().get();
-		assertNotNull(partyExternalReferenceToCounterpartyEnumMap);
-		assertEquals(CounterpartyEnum.PARTY_1, partyExternalReferenceToCounterpartyEnumMap.get(PAYER_PARTY_REF));
-		assertEquals(CounterpartyEnum.PARTY_2, partyExternalReferenceToCounterpartyEnumMap.get(RECEIVER_PARTY_REF));
 	}
 
 	@Test
