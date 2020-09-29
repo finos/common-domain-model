c65321f10ce43e4432e84a99a2a9024ca47fad7c
Hugo Hills <hugo.hills@regnosys.com>
2020-09-29 20:19:17 +0100

    Update DSL to version 3.3.0, with changes related to MetaFields

--- src/test/java/org/isda/cdm/functions/ResolveContractualProductTest.java
+++ src/test/java/org/isda/cdm/functions/ResolveContractualProductTest.java
@@ -23,7 +23,7 @@ import com.google.common.io.Resources;
 import com.google.inject.Inject;
 import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
 import com.rosetta.model.lib.GlobalKey;
-import com.rosetta.model.lib.meta.MetaFieldsI;
+import com.rosetta.model.lib.meta.GlobalKeyFields;
 
 import cdm.legalagreement.contract.Contract;
 import cdm.observable.asset.QuantityNotation;
@@ -268,7 +268,7 @@ class ResolveContractualProductTest extends AbstractFunctionTest {
 	private boolean externalKeyMatches(GlobalKey o, String key) {
 		return Optional.ofNullable(o)
 				.map(GlobalKey::getMeta)
-				.map(MetaFieldsI::getExternalKey)
+				.map(GlobalKeyFields::getExternalKey)
 				.map(key::equals)
 				.orElse(false);
 	}
