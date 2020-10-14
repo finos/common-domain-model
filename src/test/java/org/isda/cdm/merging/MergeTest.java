29cc27323c52411bf5204756d6b4698d338c3226
Hugo Hills <hugo.hills@regnosys.com>
2020-10-14 16:23:22 +0100

    Add merging for lists, and more test cases

--- src/test/java/org/isda/cdm/merging/MergeTest.java
+++ src/test/java/org/isda/cdm/merging/MergeTest.java
@@ -1,10 +1,12 @@
 package org.isda.cdm.merging;
 
+import cdm.base.staticdata.party.Party;
 import cdm.product.template.Product;
 import com.google.common.io.Resources;
 import com.regnosys.rosetta.common.merging.SimpleBuilderMerger;
 import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
 import com.regnosys.rosetta.common.util.PathCollectorBuilderProcessor;
+import com.rosetta.model.lib.RosettaModelObject;
 import com.rosetta.model.lib.path.RosettaPath;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -14,8 +16,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
 
+import static cdm.base.staticdata.party.Party.*;
 import static cdm.product.template.Product.ProductBuilder;
-import static cdm.product.template.Product.builder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -23,49 +25,64 @@ class MergeTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MergeTest.class);
 
-	private static final String INPUT_1 = "merging/input1.json";
-	private static final String INPUT_2 = "merging/input2.json";
-	private static final String INPUT_3 = "merging/input3.json";
-	private static final String EXPECTED_RESULT = "merging/expected-result.json";
+	private static final String PRODUCT_TEMPLATE = "merging/product-template.json";
+	private static final String PRODUCT_1 = "merging/product1.json";
+	private static final String PRODUCT_2 = "merging/product2.json";
+	private static final String PRODUCT_EXPECTED_RESULT = "merging/product-expected-result.json";
+
+	private static final String PARTY_TEMPLATE = "merging/party-template.json";
+	private static final String PARTY = "merging/party.json";
+	private static final String PARTY_EXPECTED_RESULT = "merging/party-expected-result.json";
 
 	@Test
-	void shouldMergeInputObjects() throws IOException {
-		ProductBuilder input1 = getProduct(INPUT_1);
-		ProductBuilder input2 = getProduct(INPUT_2);
+	void shouldMergeProductObjects() throws IOException {
+		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
+		ProductBuilder input1 = getObject(Product.class, PRODUCT_1).toBuilder();
 
-		ProductBuilder merged = builder()
-				.merge(input1, input2, new SimpleBuilderMerger());
+		ProductBuilder merged = Product.builder().merge(template, input1, new SimpleBuilderMerger());
 
-		assertEquals(getProduct(EXPECTED_RESULT), merged);
+		ProductBuilder expected = getObject(Product.class, PRODUCT_EXPECTED_RESULT).toBuilder();
+		assertEquals(expected, merged);
 	}
 
 	@Test
-	void shouldMergeInputObjectsReverse() throws IOException {
-		ProductBuilder input1 = getProduct(INPUT_1);
-		ProductBuilder input2 = getProduct(INPUT_2);
+	void shouldMergeProductObjectReverseParameterOrder() throws IOException {
+		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
+		ProductBuilder input1 = getObject(Product.class, PRODUCT_1).toBuilder();
 
-		ProductBuilder merged = builder()
-				.merge(input2, input1, new SimpleBuilderMerger());
+		ProductBuilder merged = Product.builder().merge(input1, template, new SimpleBuilderMerger());
 
-		assertEquals(getProduct(EXPECTED_RESULT), merged);
+		ProductBuilder expected = getObject(Product.class, PRODUCT_EXPECTED_RESULT).toBuilder();
+		assertEquals(expected, merged);
 	}
 
 	@Test
-	void shouldMergeInputObjectsClash() throws IOException {
-		ProductBuilder input1 = getProduct(INPUT_1);
-		ProductBuilder input3 = getProduct(INPUT_3);
+	void shouldMergeProductObjectsClash() throws IOException {
+		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
+		ProductBuilder input2 = getObject(Product.class, PRODUCT_2).toBuilder();
 
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
-				() -> builder().merge(input1, input3, new SimpleBuilderMerger()));
+				() -> Product.builder().merge(template, input2, new SimpleBuilderMerger()));
 
-		assertEquals("Attempting to merge 2 different basic values [left=INITIAL_BUYER, right=EITHER, type=CallingPartyEnum]", thrown.getMessage());
+		assertEquals("Attempting to merge 2 different basic values [left=false, right=true, type=Boolean]", thrown.getMessage());
 	}
 
-	private ProductBuilder getProduct(String resourceName) throws IOException {
+	@Test
+	void shouldMergePartyObjects() throws IOException {
+		PartyBuilder template = getObject(Party.class, PARTY_TEMPLATE).toBuilder();
+		PartyBuilder input = getObject(Party.class, PARTY).toBuilder();
+
+		PartyBuilder merged = Party.builder().merge(template, input, new SimpleBuilderMerger());
+
+		PartyBuilder expected = getObject(Party.class, PARTY_EXPECTED_RESULT).toBuilder();
+		assertEquals(expected, merged);
+	}
+
+	private <T extends RosettaModelObject> T getObject(Class<T> clazz, String resourceName) throws IOException {
 		URL url = Resources.getResource(resourceName);
 		String json = Resources.toString(url, Charset.defaultCharset());
-		return RosettaObjectMapper.getNewRosettaObjectMapper().readValue(json, Product.class).toBuilder();
+		return RosettaObjectMapper.getNewRosettaObjectMapper().readValue(json, clazz);
 	}
 
 	@SuppressWarnings("unused")
