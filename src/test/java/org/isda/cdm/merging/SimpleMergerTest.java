b62d9c5f7cad9d11bae13de44daf33343a13d798
Hugo Hills <hugo.hills@regnosys.com>
2020-10-15 16:55:11 +0100

    Working

--- src/test/java/org/isda/cdm/merging/SimpleMergerTest.java
+++ src/test/java/org/isda/cdm/merging/SimpleMergerTest.java
@@ -3,7 +3,7 @@ package org.isda.cdm.merging;
 import cdm.base.staticdata.party.Party;
 import cdm.product.template.Product;
 import com.google.common.io.Resources;
-import com.regnosys.rosetta.common.merging.SimpleBuilderMerger;
+import com.regnosys.rosetta.common.merging.SimpleMerger;
 import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
 import com.regnosys.rosetta.common.util.PathCollectorBuilderProcessor;
 import com.rosetta.model.lib.RosettaModelObject;
@@ -16,56 +16,56 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
 
-import static cdm.base.staticdata.party.Party.*;
+import static cdm.base.staticdata.party.Party.PartyBuilder;
 import static cdm.product.template.Product.ProductBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MergeTest {
+class SimpleMergerTest {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MergeTest.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMergerTest.class);
 
 	private static final String PRODUCT_TEMPLATE = "merging/product-template.json";
-	private static final String PRODUCT_1 = "merging/product1.json";
-	private static final String PRODUCT_2 = "merging/product2.json";
-	private static final String PRODUCT_EXPECTED_RESULT = "merging/product-expected-result.json";
+	private static final String PRODUCT_1 = "merging/product-unmerged1.json";
+	private static final String PRODUCT_2 = "merging/product-unmerged2.json";
+	private static final String PRODUCT_MERGED = "merging/product-merged.json";
 
 	private static final String PARTY_TEMPLATE = "merging/party-template.json";
-	private static final String PARTY = "merging/party.json";
-	private static final String PARTY_EXPECTED_RESULT = "merging/party-expected-result.json";
+	private static final String PARTY = "merging/party-unmerged.json";
+	private static final String PARTY_MERGED = "merging/party-merged.json";
 
 	@Test
 	void shouldMergeProductObjects() throws IOException {
 		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
-		ProductBuilder input1 = getObject(Product.class, PRODUCT_1).toBuilder();
+		ProductBuilder input = getObject(Product.class, PRODUCT_1).toBuilder();
 
-		ProductBuilder merged = Product.builder().merge(template, input1, new SimpleBuilderMerger());
+		Product merged = SimpleMerger.merge(input, template).build();
 
-		ProductBuilder expected = getObject(Product.class, PRODUCT_EXPECTED_RESULT).toBuilder();
+		Product expected = getObject(Product.class, PRODUCT_MERGED);
 		assertEquals(expected, merged);
 	}
 
 	@Test
 	void shouldMergeProductObjectReverseParameterOrder() throws IOException {
 		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
-		ProductBuilder input1 = getObject(Product.class, PRODUCT_1).toBuilder();
+		ProductBuilder input = getObject(Product.class, PRODUCT_1).toBuilder();
 
-		ProductBuilder merged = Product.builder().merge(input1, template, new SimpleBuilderMerger());
+		Product merged = SimpleMerger.merge(input, template).build();
 
-		ProductBuilder expected = getObject(Product.class, PRODUCT_EXPECTED_RESULT).toBuilder();
+		Product expected = getObject(Product.class, PRODUCT_MERGED);
 		assertEquals(expected, merged);
 	}
 
 	@Test
 	void shouldMergeProductObjectsClash() throws IOException {
 		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
-		ProductBuilder input2 = getObject(Product.class, PRODUCT_2).toBuilder();
+		ProductBuilder input = getObject(Product.class, PRODUCT_2).toBuilder();
 
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
-				() -> Product.builder().merge(template, input2, new SimpleBuilderMerger()));
+				() -> SimpleMerger.merge(input, template));
 
-		assertEquals("Attempting to merge 2 different basic values [left=false, right=true, type=Boolean]", thrown.getMessage());
+		assertEquals("Attempting to merge 2 different basic values [left=true, right=false, type=Boolean]", thrown.getMessage());
 	}
 
 	@Test
@@ -73,9 +73,9 @@ class MergeTest {
 		PartyBuilder template = getObject(Party.class, PARTY_TEMPLATE).toBuilder();
 		PartyBuilder input = getObject(Party.class, PARTY).toBuilder();
 
-		PartyBuilder merged = Party.builder().merge(template, input, new SimpleBuilderMerger());
+		Party merged = SimpleMerger.merge(input, template).build();
 
-		PartyBuilder expected = getObject(Party.class, PARTY_EXPECTED_RESULT).toBuilder();
+		Party expected = getObject(Party.class, PARTY_MERGED);
 		assertEquals(expected, merged);
 	}
 
---
+++ src/test/java/org/isda/cdm/merging/SimpleUnmergerTest.java
@@ -0,0 +1,56 @@
+package org.isda.cdm.merging;
+
+import cdm.base.staticdata.party.Party;
+import cdm.product.template.Product;
+import com.google.common.io.Resources;
+import com.regnosys.rosetta.common.merging.SimpleUnmerger;
+import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
+import com.rosetta.model.lib.RosettaModelObject;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import static cdm.base.staticdata.party.Party.PartyBuilder;
+import static cdm.product.template.Product.ProductBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleUnmergerTest {
+
+	private static final String PRODUCT_TEMPLATE = "merging/product-template.json";
+	private static final String PRODUCT_MERGED = "merging/product-merged.json";
+	private static final String PRODUCT_UNMERGED = "merging/product-unmerged1.json";
+
+	private static final String PARTY_TEMPLATE = "merging/party-template.json";
+	private static final String PARTY_MERGED = "merging/party-merged.json";
+	private static final String PARTY_UNMERGED = "merging/party-unmerged.json";
+
+	@Test
+	void shouldMergeProductObjects() throws IOException {
+		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
+		ProductBuilder merged = getObject(Product.class, PRODUCT_MERGED).toBuilder();
+
+		Product unmerged = SimpleUnmerger.merge(merged, template).build();
+
+		Product expected = getObject(Product.class, PRODUCT_UNMERGED);
+		assertEquals(expected, unmerged);
+	}
+
+	@Test
+	void shouldMergePartyObjects() throws IOException {
+		PartyBuilder template = getObject(Party.class, PARTY_TEMPLATE).toBuilder();
+		PartyBuilder merged = getObject(Party.class, PARTY_MERGED).toBuilder();
+
+		Party unmerged = SimpleUnmerger.merge(merged, template).build();
+
+		Party expected = getObject(Party.class, PARTY_UNMERGED);
+		assertEquals(expected, unmerged);
+	}
+
+	private <T extends RosettaModelObject> T getObject(Class<T> clazz, String resourceName) throws IOException {
+		URL url = Resources.getResource(resourceName);
+		String json = Resources.toString(url, Charset.defaultCharset());
+		return RosettaObjectMapper.getNewRosettaObjectMapper().readValue(json, clazz);
+	}
+}
