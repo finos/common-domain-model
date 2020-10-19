9b6b1f073b8d2363a144347f01ac82b1ec84b36d
Hugo Hills <hugo.hills@regnosys.com>
2020-10-19 23:10:41 +0100

    Unit tests and tidy up

--- src/test/java/org/isda/cdm/merging/SimpleMergerTest.java
+++ src/test/java/org/isda/cdm/merging/SimpleMergerTest.java
@@ -5,12 +5,8 @@ import cdm.product.template.Product;
 import com.google.common.io.Resources;
 import com.regnosys.rosetta.common.merging.SimpleMerger;
 import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
-import com.regnosys.rosetta.common.util.PathCollectorBuilderProcessor;
 import com.rosetta.model.lib.RosettaModelObject;
-import com.rosetta.model.lib.path.RosettaPath;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -22,8 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SimpleMergerTest {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMergerTest.class);
 
 	private static final String PRODUCT_TEMPLATE = "merging/product-template.json";
 	private static final String PRODUCT_1 = "merging/product-unmerged1.json";
@@ -39,7 +33,7 @@ class SimpleMergerTest {
 		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
 		ProductBuilder input = getObject(Product.class, PRODUCT_1).toBuilder();
 
-		Product merged = new SimpleMerger().merge(input, template).build();
+		Product merged = new SimpleMerger().run(input, template).build();
 
 		Product expected = getObject(Product.class, PRODUCT_MERGED);
 		assertEquals(expected, merged);
@@ -50,7 +44,7 @@ class SimpleMergerTest {
 		ProductBuilder template = getObject(Product.class, PRODUCT_TEMPLATE).toBuilder();
 		ProductBuilder input = getObject(Product.class, PRODUCT_1).toBuilder();
 
-		Product merged = new SimpleMerger().merge(input, template).build();
+		Product merged = new SimpleMerger().run(input, template).build();
 
 		Product expected = getObject(Product.class, PRODUCT_MERGED);
 		assertEquals(expected, merged);
@@ -63,7 +57,7 @@ class SimpleMergerTest {
 
 		IllegalArgumentException thrown = assertThrows(
 				IllegalArgumentException.class,
-				() -> new SimpleMerger().merge(input, template));
+				() -> new SimpleMerger().run(input, template));
 
 		assertEquals("Attempting to merge 2 different basic values [left=true, right=false, type=Boolean]", thrown.getMessage());
 	}
@@ -73,7 +67,7 @@ class SimpleMergerTest {
 		PartyBuilder template = getObject(Party.class, PARTY_TEMPLATE).toBuilder();
 		PartyBuilder input = getObject(Party.class, PARTY).toBuilder();
 
-		Party merged = new SimpleMerger().merge(input, template).build();
+		Party merged = new SimpleMerger().run(input, template).build();
 
 		Party expected = getObject(Party.class, PARTY_MERGED);
 		assertEquals(expected, merged);
@@ -83,15 +77,5 @@ class SimpleMergerTest {
 		URL url = Resources.getResource(resourceName);
 		String json = Resources.toString(url, Charset.defaultCharset());
 		return RosettaObjectMapper.getNewRosettaObjectMapper().readValue(json, clazz);
-	}
-
-	@SuppressWarnings("unused")
-	private void printPathsAndValues(Product product) {
-		PathCollectorBuilderProcessor processor = new PathCollectorBuilderProcessor();
-		ProductBuilder builder = product.toBuilder();
-		builder.process(RosettaPath.valueOf(product.getClass().getSimpleName()), processor);
-		processor.report()
-				.getCollectedPaths()
-				.forEach((key, value) -> LOGGER.debug("{} {}", key.buildPath(), value));
 	}
 }
