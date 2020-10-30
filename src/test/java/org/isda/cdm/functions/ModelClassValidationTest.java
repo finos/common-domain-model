4112e4e102bf0b9197d0b8f70202047c09180629
jim-h-wang <jim.wang@regnosys.com>
2020-10-30 16:40:35 +0000

    Fix all tests

--- src/test/java/org/isda/cdm/functions/ModelClassValidationTest.java
+++ src/test/java/org/isda/cdm/functions/ModelClassValidationTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import cdm.event.common.TradeNew;
 import cdm.event.common.TradeState;
+import cdm.event.common.meta.TradeNewMeta;
 import cdm.event.common.meta.TradeStateMeta;
 import org.junit.jupiter.api.Test;
 
@@ -17,9 +18,8 @@ public class ModelClassValidationTest {
 
 	@Test
 	void checkErrorMessages() {
-		TradeState tradeState =
-            TradeState.builder()
-                .setTradeBuilder(TradeNew.builder()
+		TradeNew tradeState =
+            TradeNew.builder()
                     .addPartyRole(PartyRole.builder()
                             .setPartyReference(ReferenceWithMetaParty.builder().setExternalReference("party1").build())
                             .setRole(PartyRoleEnum.DETERMINING_PARTY)
@@ -27,12 +27,12 @@ public class ModelClassValidationTest {
                     .addPartyRole(PartyRole.builder()
                             .setPartyReference(ReferenceWithMetaParty.builder().setExternalReference("party1").build())
                             .setRole(PartyRoleEnum.DETERMINING_PARTY)
-                            .build()))
+                            .build())
                 .build();
 
-		ValidationResult<? super TradeState> result = new TradeStateMeta().validator().validate(null, tradeState);
+		ValidationResult<? super TradeNew> result = new TradeNewMeta().validator().validate(null, tradeState);
 		assertEquals(
-				"contractIdentifier - Expected cardinality lower bound of [1] found [0]; tradeDate - Expected cardinality lower bound of [1] found [0]; tradableProduct - Expected cardinality lower bound of [1] found [0]",
+				"identifier - Expected cardinality lower bound of [1] found [0]; tradeDate - Expected cardinality lower bound of [1] found [0]; tradableProduct - Expected cardinality lower bound of [1] found [0]",
 				result.getFailureReason().orElse("No error message"));
 	}
 
