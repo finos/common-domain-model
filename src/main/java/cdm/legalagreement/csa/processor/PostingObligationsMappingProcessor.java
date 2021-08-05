93b104a51157bc481fed0f3a22da1942f9736a58
David Al-Kanani <david.al-kanani@regnosys.com>
2021-08-05 16:59:09 +0100

    map fx haircut value

--- src/main/java/cdm/legalagreement/csa/processor/PostingObligationsMappingProcessor.java
+++ src/main/java/cdm/legalagreement/csa/processor/PostingObligationsMappingProcessor.java
@@ -2,16 +2,17 @@ package cdm.legalagreement.csa.processor;
 
 import cdm.legalagreement.csa.PostingObligations;
 import cdm.legalagreement.csa.PostingObligationsElection;
-import com.regnosys.rosetta.common.translation.MappingContext;
-import com.regnosys.rosetta.common.translation.MappingProcessor;
-import com.regnosys.rosetta.common.translation.Path;
+import com.regnosys.rosetta.common.translation.*;
 import com.rosetta.model.lib.RosettaModelObjectBuilder;
 import com.rosetta.model.lib.path.RosettaPath;
 import org.jetbrains.annotations.NotNull;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Optional;
 
+import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.setValueAndOptionallyUpdateMappings;
 import static org.isda.cdm.processor.IsdaCreateMappingProcessorUtils.PARTIES;
 import static org.isda.cdm.processor.IsdaCreateMappingProcessorUtils.toCounterpartyRoleEnum;
 
@@ -49,7 +50,53 @@ public class PostingObligationsMappingProcessor extends MappingProcessor {
         // if we find partyX_control_agreement and its value is 'yey' then set ExcludedCollateral to partyX_control_agreement_specify if it exists.
         setExcludedCollateralValueIfYey(synonymPath, party, postingObligationsElectionBuilder, "_control_agreement", "_control_agreement_specify");
 
+        // parses the text field in answers.partyA.chargor_posting_obligations.partyB_fx_haircut_percentage and looks for first occurrence of [d]% where d is one or more digits
+        setFxHaircutPercentage(synonymPath, party, postingObligationsElectionBuilder);
+
         return postingObligationsElectionBuilder.hasData() ? Optional.of(postingObligationsElectionBuilder.build()) : Optional.empty();
+    }
+
+    private void setFxHaircutPercentage(Path synonymPath, String party, PostingObligationsElection.PostingObligationsElectionBuilder postingObligationsElectionBuilder) {
+        Path fxHaircutPercentagePath = synonymPath.addElement(party + "_fx_haircut_percentage");
+
+        setValueAndOptionallyUpdateMappings(fxHaircutPercentagePath,
+                xmlValue -> {
+                    Optional<BigDecimal> maybeFxHaircutValue = parseFxHaircutPercentageValue(xmlValue);
+
+                    maybeFxHaircutValue.ifPresent(fxHaircutPercentage -> {
+                        postingObligationsElectionBuilder
+                                .getOrCreateEligibleCollateral(0)
+                                .getOrCreateCriteria(0)
+                                .getOrCreateTreatment()
+                                .getOrCreateValuationTreatment()
+                                .setFxHaircutPercentage(fxHaircutPercentage);
+                    });
+
+                    return maybeFxHaircutValue.isPresent();
+                },
+                getMappings(),
+                getModelPath()
+        );
+    }
+
+    private Optional<BigDecimal> parseFxHaircutPercentageValue(Object xmlValue) {
+        if (!(xmlValue instanceof String)) {
+            return Optional.empty();
+        }
+
+        String value = (String) xmlValue;
+        int intValue;
+        try {
+            intValue = Integer.parseInt(value.replaceAll(".*\\[(\\d+)\\]%.*", "$1"));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+
+        if (intValue > 100 || intValue < 0) {
+            return Optional.empty();
+        }
+        BigDecimal bdValue = BigDecimal.valueOf(intValue);
+        return Optional.of(bdValue.divide(BigDecimal.valueOf(100), RoundingMode.HALF_EVEN));
     }
 
     private void setExcludedCollateralValueIfYey(Path synonymPath, String party, PostingObligationsElection.PostingObligationsElectionBuilder postingObligationsElectionBuilder, String collateral_management_agreement, String collateral_management_agreement_specify) {
