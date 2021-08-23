6fcab50dd43329a34eb9b88ff57a5994348afcde
Hugo Hills <hugo.hills@regnosys.com>
2021-08-23 11:59:27 +0100

    Remove unnecessary cache

---
+++ src/main/java/cdm/base/datetime/functions/BusinessCenterHolidaysImpl.java
@@ -0,0 +1,43 @@
+package cdm.base.datetime.functions;
+
+import cdm.base.datetime.BusinessCenters;
+import cdm.base.datetime.DateGroup;
+import cdm.base.datetime.metafields.FieldWithMetaBusinessCenterEnum;
+import cdm.base.datetime.metafields.ReferenceWithMetaBusinessCenters;
+import com.google.inject.Inject;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class BusinessCenterHolidaysImpl extends BusinessCenterHolidays {
+
+	@Inject
+	private BusinessCenterHolidaysDataProvider dataProvider; // raw holiday lists
+
+	protected DateGroup.DateGroupBuilder doEvaluate(BusinessCenters businessCenters) {
+		return DateGroup.builder()
+				.setDates(getBusinessCenters(businessCenters).stream()
+						.filter(Objects::nonNull)
+						.map(FieldWithMetaBusinessCenterEnum::getValue)
+						.filter(Objects::nonNull)
+						.map(dataProvider::getHolidays)
+						.filter(Objects::nonNull)
+						.flatMap(Collection::stream)
+						.sorted()
+						.collect(Collectors.toList()));
+	}
+
+	// retrieve the list of business centers to a BusinessCenters structure, de-referencing if necessary
+	private List<? extends FieldWithMetaBusinessCenterEnum> getBusinessCenters(BusinessCenters bc) {
+		if (bc == null) {
+			return null;
+		}
+		ReferenceWithMetaBusinessCenters ref = bc.getBusinessCentersReference();
+		if (ref != null) {
+			return getBusinessCenters(ref.getValue());
+		}
+		return bc.getBusinessCenter();
+	}
+}
