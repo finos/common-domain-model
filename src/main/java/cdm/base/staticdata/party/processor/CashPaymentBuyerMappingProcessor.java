69578a7524287ea2335973217f37d2580af03eb3
Minesh Patel <minesh.patel@regnosys.com>
2021-09-16 14:27:22 +0100

    Correct cash payment buyer/seller

---
+++ src/main/java/cdm/base/staticdata/party/processor/CashPaymentBuyerMappingProcessor.java
@@ -0,0 +1,39 @@
+package cdm.base.staticdata.party.processor;
+
+import cdm.base.staticdata.party.BuyerSeller;
+import cdm.base.staticdata.party.CounterpartyRoleEnum;
+import cdm.legalagreement.contract.processor.PartyMappingHelper;
+import com.regnosys.rosetta.common.translation.MappingContext;
+import com.regnosys.rosetta.common.translation.MappingProcessorUtils;
+import com.regnosys.rosetta.common.translation.Path;
+import com.rosetta.model.lib.RosettaModelObjectBuilder;
+import com.rosetta.model.lib.path.RosettaPath;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.getNonNullMappedValue;
+
+/**
+ * FpML mapping processor.
+ */
+@SuppressWarnings("unused")
+public class CashPaymentBuyerMappingProcessor extends BuyerMappingProcessor {
+
+	public CashPaymentBuyerMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext mappingContext) {
+		super(modelPath, synonymPaths, mappingContext);
+	}
+
+	@Override
+	public <T> void mapBasic(Path synonymPath, Optional<T> instance, RosettaModelObjectBuilder parent) {
+		boolean isPaymentAmount = getNonNullMappedValue(synonymPath.getParent().addElement("paymentAmount")
+				.addElement("amount"), getMappings()).isPresent();
+		boolean isFixedAmount = getNonNullMappedValue(synonymPath.getParent().addElement("fixedAmount")
+				.addElement("amount"), getMappings()).isPresent();
+		if (isPaymentAmount || isFixedAmount) {
+			getNonNullMappedValue(synonymPath.getParent().addElement("paymentAmount").addElement("amount"), getMappings())
+					.ifPresent(x -> super.mapBasic(synonymPath, instance, parent));
+		}
+	}
+}
