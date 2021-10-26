050a57221a49050db4b3e4f7b15f30bf310d8e41
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2021-10-26 11:55:21 +0100

    Create_Termination and tests (#1355)
    * Create_Termination and tests
    
    * Update docs
    
    * Release note
    
    * Update RELEASE.md

--- RELEASE.md
+++ RELEASE.md
@@ -1,42 +1,13 @@
-# *DSL Syntax - Distinct keyword*
+# *Event Model - Create Termination Function*
 
 _What is being released?_
 
-This release introduces the new DSL keyword `distinct` used to remove duplicates from a list.
+This release introduces a creation function for termination and partial termination events.
 
-The `distinct` keyword can appear after an attribute with multiple cardinality in a path expression, as shown in the example below.
+_Details_
 
-- `quantity -> unitOfAmount -> currency distinct`
-
-The operation will return a subset of the list containing only distinct elements.  It’s useful for removing duplicate elements from a list, and can be combined with other syntax features such as `count` to determine if all elements of a list are equal, as shown in the example below.
-
-- `payout -> interestRatePayout -> payoutQuantity -> quantitySchedule -> initialQuantity -> unitOfAmount -> currency distinct count = 1`
-
-The product qualification function, `Qualify_BaseProduct_CrossCurrency`, has been updated to use the distinct keyword to identify when a product has a different currency specified on each `InterestRatePayout`.
-
-    func Qualify_BaseProduct_CrossCurrency: 
-        inputs: economicTerms EconomicTerms (1..1)
-        output: is_product boolean (1..1)
-        assign-output is_product:
-        Qualify_AssetClass_InterestRate_Swap(economicTerms) = True
-           and economicTerms -> payout -> interestRatePayout count = 2
-            and (
-                economicTerms -> payout -> interestRatePayout -> payoutQuantity -> quantitySchedule -> initialQuantity -> unitOfAmount -> currency distinct count = 2
-             or (
-                 economicTerms -> payout -> interestRatePayout -> payoutQuantity -> quantitySchedule -> initialQuantity -> unitOfAmount -> currency exists
-                 and economicTerms -> payout -> interestRatePayout -> payoutQuantity -> quantityMultiplier -> fxLinkedNotionalSchedule -> varyingNotionalCurrency exists
-                 and economicTerms -> payout -> interestRatePayout -> payoutQuantity -> quantitySchedule -> initialQuantity -> unitOfAmount -> currency &lt;> economicTerms -> payout -> interestRatePayout -> payoutQuantity -> quantityMultiplier -> fxLinkedNotionalSchedule -> varyingNotionalCurrency
-             )
-         )
+The function `Create_Termination` has inputs of type `TradeState` and `TerminationInstructions`, and an output of type `BusinessEvent` that contains a `QuantityChangePrimitive`, and optionally a `TransferPrimitive`.
 
 _Review Directions_
 
-In the CDM Portal, select the Textual Browser and review the following functions:
-
-- `Qualify_BaseProduct_CrossCurrency`
-- `Qualify_BaseProduct_IRSwap`
-- `Qualify_BaseProduct_Inflation`
-
-In the CDM Portal, select the User Documentation tile and navigate to the Rosetta DSL > Expression > Rosetta Path Expressions section, or review the documentation section directly:
-
-- [DSL Documentation - Distinct](https://docs.rosetta-technology.io/dsl/expressions.html#distinct)
+In the CDM Portal, select the Textual Browser and review the type and functions mentioned above.
---
+++ src/test/java/org/isda/cdm/functions/FunctionInputCreationTest.java
@@ -0,0 +1,88 @@
+package org.isda.cdm.functions;
+
+import cdm.base.math.Quantity;
+import cdm.base.math.UnitType;
+import cdm.event.common.TerminationInstruction;
+import cdm.event.common.TradeState;
+import cdm.event.workflow.WorkflowStep;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.google.common.io.Resources;
+import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
+import com.rosetta.model.lib.records.DateImpl;
+import com.rosetta.model.metafields.FieldWithMetaString;
+import com.rosetta.model.metafields.MetaFields;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import util.ResourcesUtils;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FunctionInputCreationTest {
+
+    private static final ObjectMapper STRICT_MAPPER = RosettaObjectMapper.getNewRosettaObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
+            .configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true)
+            .setNodeFactory(JsonNodeFactory.withExactBigDecimals(true));
+
+    @Test
+    void validateCreateTerminationWorkflowFuncInputJson() throws IOException {
+        RunCreateTerminationWorkflowInput actual = new RunCreateTerminationWorkflowInput(
+                getTradeState(),
+                TerminationInstruction.builder()
+                        .addTerminatedQuantity(Quantity.builder()
+                                .setAmount(BigDecimal.valueOf(0))
+                                .setUnitOfAmount(UnitType.builder().setCurrency(FieldWithMetaString.builder()
+                                        .setValue("USD")
+                                        .setMeta(MetaFields.builder()
+                                                .setScheme("http://www.fpml.org/coding-scheme/external/iso4217"))).build())
+                                .build())
+                        .setTerminationDate(DateImpl.of(2019, 12, 12)).build());
+
+        assertEquals(readResource("/cdm-sample-files/functions/termination-workflow-func-input.json"),
+                STRICT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(actual),
+                "The input JSON for termination-workflow-func-input.json has been updated (probably due to a model change). Update the input file");
+    }
+
+    @Test
+    void validateCreatePartialTerminationWorkflowFuncInputJson() throws IOException {
+        RunCreateTerminationWorkflowInput actual = new RunCreateTerminationWorkflowInput(
+                getTradeState(),
+                TerminationInstruction.builder()
+                        .addTerminatedQuantity(Quantity.builder()
+                                .setAmount(BigDecimal.valueOf(3000))
+                                .setUnitOfAmount(UnitType.builder().setCurrency(FieldWithMetaString.builder()
+                                        .setValue("USD")
+                                        .setMeta(MetaFields.builder()
+                                                .setScheme("http://www.fpml.org/coding-scheme/external/iso4217"))).build())
+                                .build())
+                        .setTerminationDate(DateImpl.of(2019, 12, 12)).build());
+
+        assertEquals(readResource("/cdm-sample-files/functions/partial-termination-workflow-func-input.json"),
+                STRICT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(actual),
+                "The input JSON for partial-termination-workflow-func-input.json has been updated (probably due to a model change). Update the input file");
+    }
+
+    @NotNull
+    private TradeState.TradeStateBuilder getTradeState() throws IOException {
+        WorkflowStep workflowStep = ResourcesUtils.getObject(WorkflowStep.class, "result-json-files/fpml-5-10/record-keeping/record-ex01-vanilla-swap.json");
+        TradeState.TradeStateBuilder tradeState = workflowStep.getBusinessEvent().getPrimitives().get(0).getContractFormation().getAfter().toBuilder();
+        tradeState.getTrade().setParty(workflowStep.getParty());
+        return tradeState;
+    }
+
+    private static String readResource(String inputJson) throws IOException {
+        //noinspection UnstableApiUsage
+        return Resources
+                .toString(Objects
+                        .requireNonNull(FunctionInputCreationTest.class.getResource(inputJson)), Charset
+                        .defaultCharset());
+    }
+}
