ab6747bce16884af462ab7dc0ac32e62d6de4981
David Al-Kanani <david.al-kanani@regnosys.com>
2021-10-26 11:57:12 +0100

    initial refactor

--- src/main/java/cdm/product/common/settlement/processor/QuantityReferenceMappingProcessor.java
+++ src/main/java/cdm/product/common/settlement/processor/QuantityReferenceMappingProcessor.java
@@ -7,9 +7,11 @@ import com.regnosys.rosetta.common.util.PathUtils;
 import com.rosetta.model.lib.RosettaModelObjectBuilder;
 import com.rosetta.model.lib.meta.Reference;
 import com.rosetta.model.lib.path.RosettaPath;
+import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.getNonNullMappedValue;
@@ -23,46 +25,59 @@ public class QuantityReferenceMappingProcessor extends MappingProcessor {
     @Override
     public void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent) {
         //synonymPath=dataDocument.trade.returnSwap.interestLeg.notional.relativeNotionalAmount
-        getNonNullMapping(getMappings(), synonymPath, "href")
+        getNotionalAmountIdMapping(synonymPath)
+                .ifPresent(notionalAmountIdMapping -> {
+                    Path returnNotionalAmountPath = notionalAmountIdMapping.getXmlPath().getParent();
+                    FieldWithMetaQuantity.FieldWithMetaQuantityBuilder quantityBuilder = (FieldWithMetaQuantity.FieldWithMetaQuantityBuilder) builder.get(0);
+
+                    mapAmount(returnNotionalAmountPath, quantityBuilder);
+
+                    mapCurrency(returnNotionalAmountPath, quantityBuilder);
+                });
+    }
+
+    @NotNull
+    private Optional<Mapping> getNotionalAmountIdMapping(Path synonymPath) {
+        return getNonNullMapping(getMappings(), synonymPath, "href")
                 .flatMap(relativeNotionalAmountHrefMapping -> getMappings().stream()
                         .filter(mapping -> mapping.getXmlPath().endsWith(Path.parse("notionalAmount.id")))
                         .filter(m -> m.getXmlValue().equals(relativeNotionalAmountHrefMapping.getXmlValue()))
-                        .findFirst())
-                .ifPresent(notionalAmountIdMapping -> {
-                    Path returnNotionalAmountPath = notionalAmountIdMapping.getXmlPath().getParent();
-                    Path amountPath = returnNotionalAmountPath.addElement("amount");
-                    FieldWithMetaQuantity.FieldWithMetaQuantityBuilder quantityBuilder = (FieldWithMetaQuantity.FieldWithMetaQuantityBuilder) builder.get(0);
+                        .findFirst());
+    }
 
-                    getNonNullMappedValue(amountPath, getMappings()).ifPresent(amount -> {
-                        quantityBuilder.getOrCreateValue().setAmount(new BigDecimal(amount));
+    private void mapCurrency(Path returnNotionalAmountPath, FieldWithMetaQuantity.FieldWithMetaQuantityBuilder quantityBuilder) {
+        Path currencyDummyPath = returnNotionalAmountPath.addElement("currency");
 
-                        Path modelPath = PriceQuantityHelper.incrementPathElementIndex(PathUtils.toPath(getModelPath()), "quantity", 0);
+        Consumer<String> currencySetter = value -> quantityBuilder.getOrCreateValue().setUnitOfAmount(UnitType.builder().setCurrencyValue(value).build());
 
-                        Path dummyAmountPath = Path.parse("dummy").append(amountPath);
-                        getMappings()
-                                .stream()
-                                .filter(x -> x.getXmlPath().endsWith("relativeNotionalAmount", "href"))
-                                .filter(x -> x.getRosettaValue() instanceof Reference.ReferenceBuilder)
-                                .findFirst()
-                                .ifPresent(reference -> {
-                                    getMappings().add(new Mapping(dummyAmountPath, amount, reference.getRosettaPath(), reference.getRosettaValue(), null, false, true, false));
-                                    getMappings().remove(reference);
-                                });
+        MappingProcessorUtils.setValueAndUpdateMappings(currencyDummyPath, currencySetter, getMappings(), getModelPath());
+    }
 
-                        getMappings().add(new Mapping(dummyAmountPath, amount, modelPath, amount, null, false, true, false));
+    private void mapAmount(Path returnNotionalAmountPath, FieldWithMetaQuantity.FieldWithMetaQuantityBuilder quantityBuilder) {
+        Path amountPath = returnNotionalAmountPath.addElement("amount");
+        getNonNullMappedValue(amountPath, getMappings()).ifPresent(amount -> {
+            quantityBuilder.getOrCreateValue().setAmount(new BigDecimal(amount));
+
+            Path modelPath = PriceQuantityHelper.incrementPathElementIndex(PathUtils.toPath(getModelPath()), "quantity", 0);
+
+            Path dummyAmountPath = Path.parse("dummy").append(amountPath);
+            getMappings()
+                    .stream()
+                    .filter(x -> x.getXmlPath().endsWith("relativeNotionalAmount", "href"))
+                    .filter(x -> x.getRosettaValue() instanceof Reference.ReferenceBuilder)
+                    .findFirst()
+                    .ifPresent(reference -> {
+                        getMappings().add(new Mapping(dummyAmountPath, amount, reference.getRosettaPath(), reference.getRosettaValue(), null, false, true, false));
+                        getMappings().remove(reference);
                     });
 
-                    Path currencyDummyPath = returnNotionalAmountPath.addElement("currency");
-
-                    Consumer<String> currencySetter = value -> quantityBuilder.getOrCreateValue().setUnitOfAmount(UnitType.builder().setCurrencyValue(value).build());
-
-                    MappingProcessorUtils.setValueAndUpdateMappings(currencyDummyPath, currencySetter, getMappings(), getModelPath());
-                });
+            getMappings().add(new Mapping(dummyAmountPath, amount, modelPath, amount, null, false, true, false));
+        });
     }
 
 
     // 1: Fix model path to have the index for quantity - done
     // 2: Get reference object stamped onto interest rate payout - done
     // 3: Hack synonym path so reference engine can differentiate between the two references we have now (ie quantity-3 and quantity-2) - done
-    // 4: Map the currency
+    // 4: Map the currency - done
 }
