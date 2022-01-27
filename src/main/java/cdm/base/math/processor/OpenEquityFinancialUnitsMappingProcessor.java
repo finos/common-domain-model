83199e89681a13a4ad78b208deaa27f74bbcb3b6
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2022-01-27 14:43:33 +0000

    Total notional quantity mapping for commodities (#1458)

--- src/main/java/cdm/base/math/processor/OpenEquityFinancialUnitsMappingProcessor.java
+++ src/main/java/cdm/base/math/processor/OpenEquityFinancialUnitsMappingProcessor.java
@@ -8,27 +8,23 @@ import com.regnosys.rosetta.common.translation.Path;
 import com.rosetta.model.lib.RosettaModelObjectBuilder;
 import com.rosetta.model.lib.path.RosettaPath;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.getNonNullMappedValue;
 import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.getNonNullMapping;
 
-public class OpenEquityFinancialUnitsMappingProcessor extends MappingProcessor  {
-    public OpenEquityFinancialUnitsMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext context) {
-        super(modelPath, synonymPaths, context);
-    }
+@SuppressWarnings("unused")
+public class OpenEquityFinancialUnitsMappingProcessor extends MappingProcessor {
 
-    @Override
-    public <T> void mapBasic(Path synonymPath, Collection<? extends T> instance, RosettaModelObjectBuilder parent) {
-        super.mapBasic(synonymPath, instance, parent);
-    }
+	public OpenEquityFinancialUnitsMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext context) {
+		super(modelPath, synonymPaths, context);
+	}
 
-    @Override
-    public <T> void mapBasic(Path openUnitPath, Optional<T> instance, RosettaModelObjectBuilder parent) {
-        getNonNullMapping(getMappings(), openUnitPath)
-                .flatMap(x -> getNonNullMappedValue(getMappings(), openUnitPath.getParent().addElement("equity")))
-                .ifPresent(x -> ((UnitType.UnitTypeBuilder) parent) .setFinancialUnit(FinancialUnitEnum.SHARE));
-    }
+	@Override
+	public <T> void mapBasic(Path openUnitPath, Optional<T> instance, RosettaModelObjectBuilder parent) {
+		getNonNullMapping(getMappings(), openUnitPath)
+				.flatMap(x -> getNonNullMappedValue(getMappings(), openUnitPath.getParent().addElement("equity")))
+				.ifPresent(x -> ((UnitType.UnitTypeBuilder) parent).setFinancialUnit(FinancialUnitEnum.SHARE));
+	}
 }
---
+++ src/main/java/cdm/product/common/settlement/processor/TotalNotionalQuantityMappingProcessor.java
@@ -0,0 +1,80 @@
+package cdm.product.common.settlement.processor;
+
+import cdm.base.math.CapacityUnitEnum;
+import cdm.base.math.Quantity;
+import cdm.base.math.UnitType;
+import cdm.base.math.metafields.FieldWithMetaQuantity;
+import cdm.product.common.settlement.PriceQuantity;
+import com.regnosys.rosetta.common.translation.MappingContext;
+import com.regnosys.rosetta.common.translation.MappingProcessor;
+import com.regnosys.rosetta.common.translation.MappingProcessorUtils;
+import com.regnosys.rosetta.common.translation.Path;
+import com.regnosys.rosetta.common.util.PathUtils;
+import com.rosetta.model.lib.RosettaModelObjectBuilder;
+import com.rosetta.model.lib.path.RosettaPath;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static cdm.product.common.settlement.processor.PriceQuantityHelper.incrementPathElementIndex;
+import static cdm.product.common.settlement.processor.PriceQuantityHelper.toReferencableQuantityBuilder;
+import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.getNonNullMappedValue;
+import static com.regnosys.rosetta.common.util.PathUtils.toPath;
+
+/**
+ * FpML mapping processor.
+ */
+@SuppressWarnings("unused")
+public class TotalNotionalQuantityMappingProcessor extends MappingProcessor {
+
+	public TotalNotionalQuantityMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext context) {
+		super(modelPath, synonymPaths, context);
+	}
+
+	@Override
+	public void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builders, RosettaModelObjectBuilder parent) {
+		List<FieldWithMetaQuantity.FieldWithMetaQuantityBuilder> quantityBuilders = filterEmpty(builders);
+
+		getTotalNotionalQuantity(synonymPath, quantityBuilders.size()).ifPresent(totalNotionalQuantity -> {
+			// add to quantity list
+			quantityBuilders.add(toReferencableQuantityBuilder(totalNotionalQuantity));
+			// set Quantity list on PriceQuantity
+			((PriceQuantity.PriceQuantityBuilder) parent).setQuantity(quantityBuilders);
+		});
+	}
+
+	@NotNull
+	private List<FieldWithMetaQuantity.FieldWithMetaQuantityBuilder> filterEmpty(List<? extends RosettaModelObjectBuilder> builders) {
+		return (List<FieldWithMetaQuantity.FieldWithMetaQuantityBuilder>) builders.stream()
+				.filter(RosettaModelObjectBuilder::hasData)
+				.collect(Collectors.toList());
+	}
+
+	private Optional<Quantity.QuantityBuilder> getTotalNotionalQuantity(Path synonymPath, int index) {
+		Quantity.QuantityBuilder quantity = Quantity.builder();
+
+		Path baseModelPath = toPath(getModelPath()).addElement("amount");
+		Path mappedModelPath = incrementPathElementIndex(baseModelPath, "quantity", index);
+
+		MappingProcessorUtils.setValueAndUpdateMappings(synonymPath,
+				(xmlValue) -> quantity
+						.setAmount(new BigDecimal(xmlValue))
+						.setUnitOfAmount(UnitType.builder().setCapacityUnit(getCapacityUnitEnum(synonymPath))),
+				getMappings(),
+				PathUtils.toRosettaPath(mappedModelPath));
+
+		return quantity.hasData() ? Optional.of(quantity) : Optional.empty();
+	}
+
+	@Nullable
+	private CapacityUnitEnum getCapacityUnitEnum(Path baseSynonymPath) {
+		Path quantityUnitPath = baseSynonymPath.getParent().addElement("notionalQuantity").addElement("quantityUnit");
+		return getNonNullMappedValue(quantityUnitPath, getMappings())
+				.flatMap(xmlValue -> getSynonymToEnumMap().getEnumValueOptional(CapacityUnitEnum.class, xmlValue))
+				.orElse(null);
+	}
+}
