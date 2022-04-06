d9bfde1caa9e4294c76ca6cf4ecddcdcfd4df892
hugohills <hugohills@hotmail.com>
2022-04-06 18:09:55 +0100

    Revert accidental commit

--- isda-demo/src/main/java/com/regnosys/cdm/example/Serialisation.java
+++ isda-demo/src/main/java/com/regnosys/cdm/example/Serialisation.java
@@ -1,7 +1,6 @@
 package com.regnosys.cdm.example;
 
 import cdm.product.asset.InterestRatePayout;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
 
@@ -20,29 +19,30 @@ public class Serialisation {
 
     public static void main(String[] args) throws Exception {
 
-        shouldSerialiseAndDeserialiseObject();
-    }
+        // Instantiate the pre-configured Rosetta ObjectMapper
+        //
+        ObjectMapper rosettaObjectMapper = RosettaObjectMapper.getDefaultRosettaObjectMapper();
 
-    private static void shouldSerialiseAndDeserialiseObject() throws JsonProcessingException {
         // Create an instance of a Fixed Rate Payout CDM object
+        //
         InterestRatePayout fixedRatePayout = InterestRatePayoutCreation.getFixedRatePayout(BigDecimal.valueOf(0.05));
 
-
-        // Instantiate the pre-configured Rosetta ObjectMapper
-        ObjectMapper rosettaObjectMapper = RosettaObjectMapper.getDefaultRosettaObjectMapper();
         // Serialise to JSON
+        //
         String json = rosettaObjectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(fixedRatePayout);
 
         System.out.println("Serialise to JSON");
         System.out.println(json);
 
         // Deserialize back to Java
+        //
         InterestRatePayout deserializedObject = rosettaObjectMapper.readValue(json, fixedRatePayout.getClass());
 
         System.out.println("Deserialize to Java");
         System.out.println(deserializedObject.toString());
 
         // Compare the original and deserialized CDM Java objects
+        //
         assertThat("The fixedRatePayout and serialized-then-deserialized objects are the same",
                 fixedRatePayout, equalTo(deserializedObject));
     }
