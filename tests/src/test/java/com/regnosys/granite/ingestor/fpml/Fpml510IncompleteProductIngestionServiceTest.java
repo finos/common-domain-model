21afe8b88049a4eed6b3dd0266692c3e6a6165ef
hugohills <hugohills@hotmail.com>
2022-04-07 17:04:24 +0100

    Revert

--- tests/src/test/java/com/regnosys/granite/ingestor/fpml/Fpml510IncompleteProductIngestionServiceTest.java
+++ tests/src/test/java/com/regnosys/granite/ingestor/fpml/Fpml510IncompleteProductIngestionServiceTest.java
@@ -1,8 +1,6 @@
 package com.regnosys.granite.ingestor.fpml;
 
 import cdm.event.common.TradeState;
-import com.google.common.collect.ImmutableList;
-import com.google.common.io.Resources;
 import com.regnosys.granite.ingestor.CdmTestInitialisationUtil;
 import com.regnosys.granite.ingestor.IngestionTest;
 import com.regnosys.granite.ingestor.service.IngestionFactory;
@@ -11,7 +9,6 @@ import org.isda.cdm.CdmRuntimeModule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.provider.Arguments;
 
-import java.net.URL;
 import java.util.stream.Stream;
 
 public class Fpml510IncompleteProductIngestionServiceTest extends IngestionTest<TradeState> {
@@ -19,11 +16,6 @@ public class Fpml510IncompleteProductIngestionServiceTest extends IngestionTest<
 	private static final String INCOMPLETE_BASE = "cdm-sample-files/fpml-5-10/incomplete-products/";
 
 	private static IngestionService ingestionService;
-
-	private static ImmutableList<URL> EXPECTATION_FILES = ImmutableList.<URL>builder()
-			.add(Resources.getResource(INCOMPLETE_BASE + "variance-swaps/" + "expectations.json"))
-			.add(Resources.getResource(INCOMPLETE_BASE + "volatility-swaps/" + "expectations.json"))
-			.build();
 
 	@BeforeAll
 	static void setup() {
@@ -48,6 +40,6 @@ public class Fpml510IncompleteProductIngestionServiceTest extends IngestionTest<
 
 	@SuppressWarnings("unused")//used by the junit parameterized test
 	private static Stream<Arguments> fpMLFiles() {
-		return readExpectationsFrom(EXPECTATION_FILES);
+		return readExpectationsFromPath(INCOMPLETE_BASE);
 	}
 }
