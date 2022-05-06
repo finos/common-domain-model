4e883e676deb81431d5ff79cc3c54bd9c3b1d543
David Al-Kanani <david.al-kanani@regnosys.com>
2022-05-06 11:57:52 +0100

    use zone offset instead of zone id

--- rosetta-source/src/main/java/cdm/base/datetime/functions/NowImpl.java
+++ rosetta-source/src/main/java/cdm/base/datetime/functions/NowImpl.java
@@ -1,12 +1,12 @@
 package cdm.base.datetime.functions;
 
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 public class NowImpl extends Now {
 
 	@Override
 	protected ZonedDateTime doEvaluate() {
-		return ZonedDateTime.now(ZoneId.of("Z"));
+		return ZonedDateTime.now(ZoneOffset.UTC);
 	}
 }
