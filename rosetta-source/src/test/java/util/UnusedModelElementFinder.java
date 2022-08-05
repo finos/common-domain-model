c36c16a8555cfe38ec3d5790c37eac08f6c24b44
PayalKhanna <payal.khanna@regnosys.com>
2022-08-05 14:21:49 +0100

    creating a list of Orphaned Types to be regrouped.

--- rosetta-source/src/test/java/util/UnusedModelElementFinder.java
+++ rosetta-source/src/test/java/util/UnusedModelElementFinder.java
@@ -19,11 +19,14 @@ public class UnusedModelElementFinder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UnusedModelElementFinder.class);
     private static final Set<String> listOfTypes = new HashSet<>();
-    private static final Set<String> listOfUsedTypes = new HashSet<>();
+    private static final Set<String>  listOfUsedTypes = new HashSet<>();
     private static final Set<String> listOfUnusedTypes = new HashSet<>();
 
     private static List<RosettaModel> models;
 
+    public static void main(String[] args) throws IOException {
+        new UnusedModelElementFinder().run();
+    }
     public void run() throws IOException {
 
         ModelLoaderImpl modelLoader = new ModelLoaderImpl(ClassPathUtils.findRosettaFilePaths().stream().map(ClassPathUtils::toUrl).toArray(URL[]::new));
@@ -57,18 +60,23 @@ public class UnusedModelElementFinder {
                     .filter(Data.class::isInstance)
                     .map(Data.class::cast)
                     .forEach(dataType -> {
+                        //LOGGER.info(" Processing data type: {}", getQualifiedName(dataType));
                         listOfTypes.add(getQualifiedName(dataType));
                         dataType.getAttributes().stream()
                                 .map(RosettaTyped::getType)
                                 .filter(t -> !RosettaBuiltinType.class.isInstance(t))
-                                .forEach(attributeType -> listOfUsedTypes.add(getQualifiedName(attributeType)));
+                                .forEach(attributeType -> {
+                                    // LOGGER.info(" Processing attribute type: {}", getQualifiedName(dataType));
+                                    if (attributeType.getModel() != null)
+                                        listOfUsedTypes.add(getQualifiedName(attributeType));
+                                });
                     });
 
             model.getElements().stream()
                     .filter(RosettaEnumeration.class::isInstance)
                     .map(RosettaEnumeration.class::cast)
                     .forEach(enumeration -> {
-                        // LOGGER.info("{}", getQualifiedName(enumeration));
+                      //  LOGGER.info("Processing enumeration type {}", getQualifiedName(enumeration));
                         listOfTypes.add(getQualifiedName(enumeration));
                     });
 
@@ -76,23 +84,21 @@ public class UnusedModelElementFinder {
                     .filter(Function.class::isInstance)
                     .map(Function.class::cast)
                     .forEach(function -> {
-                        // LOGGER.info("{}.{}", function.getModel().getName(), function.getName());
-                        function.getInputs()
+                        //LOGGER.info(" Processing function types {}.{}", function.getModel().getName(), function.getName());
+                        function.getInputs().stream()
+                                .map(RosettaTyped::getType)
+                                .filter(t -> !RosettaBuiltinType.class.isInstance(t))
                                 .forEach(inputs -> {
-                                    // LOGGER.info("  Processing input Types used within function {}", inputs.getName());
-                                    listOfUsedTypes.add(inputs.getType().getName());
+                                   // LOGGER.info("  Processing input types used within function {}", getQualifiedName(inputs));
+                                    listOfUsedTypes.add(getQualifiedName(inputs));
                                 });
                         if (function.getOutput() != null) {
-                            //LOGGER.info("Processing output Types used within function {}", function.getOutput().getName());
-                            listOfUsedTypes.add(function.getOutput().getType().getName());
+                          // LOGGER.info("Processing output types used within function {}", getQualifiedName(function.getOutput().getType()));
+                            listOfUsedTypes.add(getQualifiedName(function.getOutput().getType()));
                         }
                         listOfTypes.add(function.getModel().getName().concat(function.getName()));
                     });
         }
 
-    }
-
-    public static void main(String[] args) throws IOException {
-        new UnusedModelElementFinder().run();
     }
 }
