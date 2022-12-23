c0e11021a37569019e7bebb2c355fde6894b327b
Simon Cockx <simon.cockx@regnosys.com>
2022-12-23 15:39:32 +0100

    Cleaned

--- scheme-import/src/main/java/com/regnosys/granite/schemaimport/SchemeEnumReader.java
+++ scheme-import/src/main/java/com/regnosys/granite/schemaimport/SchemeEnumReader.java
@@ -1,14 +1,9 @@
 package com.regnosys.granite.schemaimport;
 
 import com.regnosys.rosetta.rosetta.RosettaEnumValue;
-import org.genericode.xml._2004.ns.codelist._0.CodeListDocument;
 
-import javax.xml.bind.JAXBException;
-import javax.xml.stream.XMLStreamException;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 public interface SchemeEnumReader {
     List<RosettaEnumValue> generateEnumFromScheme(URL codingSchemeUrl, String codingSchemeRelativePath, String schemeLocation);
