55cde6d29af3a3a2245435315b172fe55bcdaafb
PayalKhanna <payal.khanna@regnosys.com>
2023-01-04 14:10:51 +0000

    update error message

--- rosetta-source/src/test/java/org/isda/cdm/functions/ModelClassValidationTest.java
+++ rosetta-source/src/test/java/org/isda/cdm/functions/ModelClassValidationTest.java
@@ -28,7 +28,7 @@ public class ModelClassValidationTest {
 
 		ValidationResult<? super Trade> result = new TradeMeta().validator().validate(null, tradeState);
 		assertEquals(
-				"Minimum of 1 'tradeIdentifier' is expected but found 0; Minimum of 1 'tradeDate' is expected but found 0",
+				"Minimum of 1 'tradeIdentifier' is expected but found 0; Minimum of 1 'tradeDate' is expected but found 0; Minimum of 1 'tradableProduct' is expected but found 0",
 				result.getFailureReason().orElse("No error message"));
 	}
 
