daea6641f8b6041e726937751da98e0d24947aad
minesh-s-patel <minesh-s-patel@users.noreply.github.com>
2023-06-15 16:20:54 +0100

    Migrate the generated Java artefacts to be published to maven central (#2093)

--- distribution/src/test/java/com/regnosys/rosetta/granite/distribution/VerifyCdmDistributionKotlinIntegrationTest.java
+++ distribution/src/test/java/com/regnosys/rosetta/granite/distribution/VerifyCdmDistributionKotlinIntegrationTest.java
@@ -8,19 +8,21 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.regnosys.rosetta.granite.distribution.VerifyFileUtils.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class VerifyCdmDistributionTypeScriptIntegrationTest {
+class VerifyCdmDistributionKotlinIntegrationTest {
 
     @Test
     void shouldVerifyCdmDistributionZipContainsExpectedFolders() throws IOException {
-        Optional<Path> distZipFile = getDistZipFile("target/cdm-distribution-typescript-*.zip");
+        Optional<Path> distZipFile = getDistZipFile("target/distribution-kotlin-*.zip");
         assertTrue(distZipFile.isPresent(), "CDM distribution zip not found");
 
         Map<Path, Long> distZipContents = getFolderFileCount(distZipFile.get());
 
-        long typescriptSrcFileCount = getFileCount(distZipContents, "/*/typescript");
-        assertTrue(typescriptSrcFileCount > 0, "typescript src is not found or empty");
+        long kotlinJarFileCount = getFileCount(distZipContents, "/*/lib");
+        assertThat("kotlin artifact (.jar) is not found", kotlinJarFileCount, is(2L));
 
         long rosettaSrcFileCount = getFileCount(distZipContents, "/*/common-domain-model");
         assertTrue(rosettaSrcFileCount > 0, "rosetta src is not found or empty");
