e876a33a7f0fab56a9fa32dbdc6713ae4ee0ed63
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2023-06-19 11:27:30 +0100

    Revert "Migrate the generated Java artefacts to be published to maven central (#2093)" (#2197)
    This reverts commit daea6641f8b6041e726937751da98e0d24947aad.

--- cdm-distribution/src/test/java/com/regnosys/rosetta/granite/distribution/VerifyCdmDistributionCSharp9IntegrationTest.java
+++ cdm-distribution/src/test/java/com/regnosys/rosetta/granite/distribution/VerifyCdmDistributionCSharp9IntegrationTest.java
@@ -10,17 +10,20 @@ import java.util.Optional;
 import static com.regnosys.rosetta.granite.distribution.VerifyFileUtils.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class VerifyCdmDistributionTypeScriptIntegrationTest {
+class VerifyCdmDistributionCSharp9IntegrationTest {
 
     @Test
     void shouldVerifyCdmDistributionZipContainsExpectedFolders() throws IOException {
-        Optional<Path> distZipFile = getDistZipFile("target/distribution-typescript-*.zip");
+        Optional<Path> distZipFile = getDistZipFile("target/cdm-distribution-csharp9-*.zip");
         assertTrue(distZipFile.isPresent(), "CDM distribution zip not found");
 
         Map<Path, Long> distZipContents = getFolderFileCount(distZipFile.get());
 
-        long typescriptSrcFileCount = getFileCount(distZipContents, "/*/typescript");
-        assertTrue(typescriptSrcFileCount > 0, "typescript src is not found or empty");
+        long csharp9SrcFileCount = getFileCount(distZipContents, "/*/csharp9/**");
+        assertTrue(csharp9SrcFileCount > 0, "csharp9 src is not found or empty");
+
+        long csharp9LibFileCount = getFileCount(distZipContents, "/*/lib/app");
+        assertTrue(csharp9LibFileCount > 0, "csharp9 artifacts (.dll) not found");
 
         long rosettaSrcFileCount = getFileCount(distZipContents, "/*/common-domain-model");
         assertTrue(rosettaSrcFileCount > 0, "rosetta src is not found or empty");
--- cdm-distribution/src/test/java/com/regnosys/rosetta/granite/distribution/VerifyCdmDistributionScalaIntegrationTest.java
+++ cdm-distribution/src/test/java/com/regnosys/rosetta/granite/distribution/VerifyCdmDistributionScalaIntegrationTest.java
@@ -8,19 +8,24 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.regnosys.rosetta.granite.distribution.VerifyFileUtils.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class VerifyCdmDistributionGolangIntegrationTest {
+class VerifyCdmDistributionScalaIntegrationTest {
 
     @Test
     void shouldVerifyCdmDistributionZipContainsExpectedFolders() throws IOException {
-        Optional<Path> distZipFile = getDistZipFile("target/distribution-golang-*.zip");
+        Optional<Path> distZipFile = getDistZipFile("target/cdm-distribution-scala-*.zip");
         assertTrue(distZipFile.isPresent(), "CDM distribution zip not found");
 
         Map<Path, Long> distZipContents = getFolderFileCount(distZipFile.get());
 
-        long golangSrcFileCount = getFileCount(distZipContents, "/*/golang/**");
-        assertTrue(golangSrcFileCount > 0, "golang src is not found or empty");
+        long scalaSrcFileCount = getFileCount(distZipContents, "/*/scala/**");
+        assertTrue(scalaSrcFileCount > 0, "scala src is not found or empty");
+
+        long scalaJarFileCount = getFileCount(distZipContents, "/*/lib");
+        assertThat("scala artifact (.jar) is not found", scalaJarFileCount, is(1L));
 
         long rosettaSrcFileCount = getFileCount(distZipContents, "/*/common-domain-model");
         assertTrue(rosettaSrcFileCount > 0, "rosetta src is not found or empty");
