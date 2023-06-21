410c4f52c0c7ebd6641ca4d2ad3fc4cbb9cd791e
Minesh Patel <minesh.patel@regnosys.com>
2023-06-21 16:00:18 +0100

    Force build to test staging

--- rosetta-source/src/main/resources/build-resources/create-deploy-pom.sh
+++ rosetta-source/src/main/resources/build-resources/create-deploy-pom.sh
@@ -15,9 +15,59 @@ cat > ${FILENAME} <<EOF
     <artifactId>${ARTIFACT_ID}</artifactId>
     <version>${VERSION}</version>
     <packaging>${PACKAGING}</packaging>
+
+    <name>${ARTIFACT_ID}</name>
+
+    <url>https://www.finos.org/common-domain-model</url>
+
     <scm>
         <developerConnection>scm:git:https://github.com/finos/common-domain-model</developerConnection>
+        <connection>scm:git:git://github.com/finos/common-domain-model.git</connection>
         <tag>HEAD</tag>
+        <url>https://github.com/finos/common-domain-model</url>
     </scm>
+
+    <description>The FINOS Common Domain Model (CDM) is a standardised, machine-readable and machine-executable blueprint for how financial products are traded and managed across the transaction lifecycle. It is represented as a domain model and distributed in open source.</description>
+
+    <organization>
+        <name>FINOS</name>
+        <url>https://finos.org</url>
+    </organization>
+
+    <licenses>
+        <license>
+            <name>Community Specification License 1.0</name>
+            <url>https://github.com/finos/common-domain-model/blob/master/LICENSE.md</url>
+        </license>
+    </licenses>
+
+    <developers>
+        <developer>
+            <id>minesh-s-patel</id>
+            <name>Minesh Patel</name>
+            <email>infra@regnosys.com</email>
+            <url>http://github.com/minesh-s-patel</url>
+            <organization>REGnosys</organization>
+            <organizationUrl>https://regnosys.com</organizationUrl>
+            <timezone>+1</timezone>
+            <roles>
+                <role>Maintainer</role>
+                <role>Developer</role>
+            </roles>
+        </developer>
+        <developer>
+            <id>hugohills-regnosys</id>
+            <name>Hugo Hills</name>
+            <email>infra@regnosys.com</email>
+            <url>http://github.com/hugohills-regnosys</url>
+            <organization>REGnosys</organization>
+            <organizationUrl>https://regnosys.com</organizationUrl>
+            <timezone>+1</timezone>
+            <roles>
+                <role>Maintainer</role>
+                <role>Developer</role>
+            </roles>
+        </developer>
+    </developers>
 </project>
 EOF
