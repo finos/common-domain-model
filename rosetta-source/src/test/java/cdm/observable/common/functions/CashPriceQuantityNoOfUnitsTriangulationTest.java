098f6ebfed28af8d6419ad759b9b56231aa12998
Simon Cockx <simon.cockx@regnosys.com>
2023-11-17 12:38:28 +0100

    Cleaned

--- rosetta-source/src/test/java/cdm/observable/common/functions/CashPriceQuantityNoOfUnitsTriangulationTest.java
+++ rosetta-source/src/test/java/cdm/observable/common/functions/CashPriceQuantityNoOfUnitsTriangulationTest.java
@@ -23,58 +23,58 @@ import static util.ResourcesUtils.getObject;
 
 public class CashPriceQuantityNoOfUnitsTriangulationTest extends AbstractFunctionTest {
 
-    private static final String EQUITY_DIR = "result-json-files/fpml-5-10/products/equity/";
+	private static final String EQUITY_DIR = "result-json-files/fpml-5-10/products/equity/";
+	
+	@Inject
+	private CashPriceQuantityNoOfUnitsTriangulation func;
 
-    @Inject
-    private CashPriceQuantityNoOfUnitsTriangulation func;
+	@Test
+	void shouldTriangulateEquityPriceNotionalAndNoOfUnitsAndReturnSuccess() throws IOException {
+		TradeState tradeState = getObject(TradeState.class, EQUITY_DIR + "eqs-ex01-single-underlyer-execution-long-form.json");
+		TradableProduct tradableProduct = tradeState.getTrade().getTradableProduct();
 
-    @Test
-    void shouldTriangulateEquityPriceNotionalAndNoOfUnitsAndReturnSuccess() throws IOException {
-        TradeState tradeState = getObject(TradeState.class, EQUITY_DIR + "eqs-ex01-single-underlyer-execution-long-form.json");
-        TradableProduct tradableProduct = tradeState.getTrade().getTradableProduct();
+		List<? extends PriceQuantity> priceQuantity = tradableProduct.getTradeLot().get(0).getPriceQuantity();
+		List<? extends NonNegativeQuantitySchedule> quantity = priceQuantity.stream()
+				.map(PriceQuantity::getQuantity)
+				.filter(Objects::nonNull)
+				.flatMap(Collection::stream)
+				.map(FieldWithMetaNonNegativeQuantitySchedule::getValue)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+		List<? extends PriceSchedule> price = priceQuantity.stream()
+				.map(PriceQuantity::getPrice)
+				.filter(Objects::nonNull)
+				.flatMap(Collection::stream)
+				.map(FieldWithMetaPriceSchedule::getValue)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+		boolean success = func.evaluate(quantity, price);
 
-        List<? extends PriceQuantity> priceQuantity = tradableProduct.getTradeLot().get(0).getPriceQuantity();
-        List<? extends NonNegativeQuantitySchedule> quantity = priceQuantity.stream()
-                .map(PriceQuantity::getQuantity)
-                .filter(Objects::nonNull)
-                .flatMap(Collection::stream)
-                .map(FieldWithMetaNonNegativeQuantitySchedule::getValue)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-        List<? extends PriceSchedule> price = priceQuantity.stream()
-                .map(PriceQuantity::getPrice)
-                .filter(Objects::nonNull)
-                .flatMap(Collection::stream)
-                .map(FieldWithMetaPriceSchedule::getValue)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-        boolean success = func.evaluate(quantity, price);
+		assertTrue(success);
+	}
 
-        assertTrue(success);
-    }
+	@Test
+	void shouldReturnSuccessNotApplicableBecauseNoOfUnitsNotDefined() throws IOException {
+		TradeState tradeState = getObject(TradeState.class, EQUITY_DIR + "eqs-ex10-short-form-interestLeg-driving-schedule-dates.json");
+		TradableProduct tradableProduct = tradeState.getTrade().getTradableProduct();
 
-    @Test
-    void shouldReturnSuccessNotApplicableBecauseNoOfUnitsNotDefined() throws IOException {
-        TradeState tradeState = getObject(TradeState.class, EQUITY_DIR + "eqs-ex10-short-form-interestLeg-driving-schedule-dates.json");
-        TradableProduct tradableProduct = tradeState.getTrade().getTradableProduct();
+		List<? extends PriceQuantity> priceQuantity = tradableProduct.getTradeLot().get(0).getPriceQuantity();
+		List<? extends NonNegativeQuantitySchedule> quantity = priceQuantity.stream()
+				.map(PriceQuantity::getQuantity)
+				.filter(Objects::nonNull)
+				.flatMap(Collection::stream)
+				.map(FieldWithMetaNonNegativeQuantitySchedule::getValue)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+		List<? extends PriceSchedule> price = priceQuantity.stream()
+				.map(PriceQuantity::getPrice)
+				.filter(Objects::nonNull)
+				.flatMap(Collection::stream)
+				.map(FieldWithMetaPriceSchedule::getValue)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+		Boolean success = func.evaluate(quantity, price);
 
-        List<? extends PriceQuantity> priceQuantity = tradableProduct.getTradeLot().get(0).getPriceQuantity();
-        List<? extends NonNegativeQuantitySchedule> quantity = priceQuantity.stream()
-                .map(PriceQuantity::getQuantity)
-                .filter(Objects::nonNull)
-                .flatMap(Collection::stream)
-                .map(FieldWithMetaNonNegativeQuantitySchedule::getValue)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-        List<? extends PriceSchedule> price = priceQuantity.stream()
-                .map(PriceQuantity::getPrice)
-                .filter(Objects::nonNull)
-                .flatMap(Collection::stream)
-                .map(FieldWithMetaPriceSchedule::getValue)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-        Boolean notDefined = func.evaluate(quantity, price);
-
-        assertNull(notDefined);
-    }
+		assertNull(success);
+	}
 }
