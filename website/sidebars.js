b95bf011f4ad300ee212344670627721641b1cba
eteridvalishvili <117176765+eteridvalishvili@users.noreply.github.com>
2024-01-10 23:15:46 -0500

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,5 +1,5 @@
 module.exports = {
   mainSidebar: {
-    'Main Menu': ["home", "cdm-overview", "common-domain-model", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping", "namespace", "eligible-collateral-representation", "repurchase-agreement-representation", "cdm-guidelines", "contribution", "download", "get-involved", "cdm-team"]
+    'Main Menu': ["home", "cdm-overview", "common-domain-model", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping", "namespace", "eligible-collateral-representation", "repurchase-agreement-representation", "cdm-guidelines", "contribution", "download", "get-involved"]
   },
 }
