12f37e57850aecc9e309035b91294e65a5ae51cd
hugohills-regnosys <hugo.hills@regnosys.com>
2024-03-20 17:23:15 +0000

    Remove unused imports

--- tests/src/test/java/com/regnosys/ingest/fpml/Fpml513ProductIngestionServiceTest.java
+++ tests/src/test/java/com/regnosys/ingest/fpml/Fpml513ProductIngestionServiceTest.java
@@ -1,29 +1,13 @@
 package com.regnosys.ingest.fpml;
 
 import cdm.event.common.TradeState;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.regnosys.ingest.test.framework.ingestor.ExpectationUtil;
-import com.regnosys.ingest.test.framework.ingestor.IngestionReport;
 import com.regnosys.ingest.test.framework.ingestor.IngestionTest;
 import com.regnosys.ingest.test.framework.ingestor.IngestionTestUtil;
 import com.regnosys.ingest.test.framework.ingestor.service.IngestionService;
-import com.regnosys.ingest.test.framework.ingestor.testing.Expectation;
-import com.regnosys.ingest.test.framework.ingestor.testing.QualificationExpectation;
-import com.regnosys.rosetta.common.util.ClassPathUtils;
-import com.regnosys.rosetta.common.util.UrlUtils;
-import org.apache.commons.io.FileUtils;
 import org.finos.cdm.CdmRuntimeModule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.provider.Arguments;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.regnosys.ingest.IngestionEnvUtil.getFpml5ConfirmationToTradeState;
