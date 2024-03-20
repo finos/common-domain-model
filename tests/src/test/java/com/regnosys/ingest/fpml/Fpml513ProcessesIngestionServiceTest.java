0cf8a43ad4ab7a31905573c212d94cc028ead78d
hugohills-regnosys <hugo.hills@regnosys.com>
2024-03-20 17:25:58 +0000

    Remove unused imports

--- tests/src/test/java/com/regnosys/ingest/fpml/Fpml513ProcessesIngestionServiceTest.java
+++ tests/src/test/java/com/regnosys/ingest/fpml/Fpml513ProcessesIngestionServiceTest.java
@@ -4,21 +4,10 @@ import cdm.event.workflow.WorkflowStep;
 import com.regnosys.ingest.test.framework.ingestor.IngestionTest;
 import com.regnosys.ingest.test.framework.ingestor.IngestionTestUtil;
 import com.regnosys.ingest.test.framework.ingestor.service.IngestionService;
-import com.regnosys.ingest.test.framework.ingestor.testing.Expectation;
-import com.regnosys.ingest.test.framework.ingestor.testing.QualificationExpectation;
-import com.regnosys.rosetta.common.util.ClassPathUtils;
-import com.regnosys.rosetta.common.util.UrlUtils;
 import org.finos.cdm.CdmRuntimeModule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.provider.Arguments;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.regnosys.ingest.IngestionEnvUtil.getFpml5ConfirmationToWorkflowStep;
