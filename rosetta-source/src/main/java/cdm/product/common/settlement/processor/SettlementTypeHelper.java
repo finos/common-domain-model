b9e31e15159743fc4b3f4332f1a28a485274c6cc
hugohills-regnosys <hugo.hills@regnosys.com>
2024-04-05 12:39:09 +0100

    Fix commodity swap settlement type mappings

--- rosetta-source/src/main/java/cdm/product/common/settlement/processor/SettlementTypeHelper.java
+++ rosetta-source/src/main/java/cdm/product/common/settlement/processor/SettlementTypeHelper.java
@@ -2,15 +2,23 @@ package cdm.product.common.settlement.processor;
 
 import cdm.product.common.settlement.SettlementTerms;
 import cdm.product.common.settlement.SettlementTypeEnum;
+import com.regnosys.rosetta.common.translation.Mapping;
 import com.regnosys.rosetta.common.translation.Path;
 import com.rosetta.model.metafields.FieldWithMetaString;
 
+import java.util.List;
 import java.util.Optional;
 
 public class SettlementTypeHelper {
 
+    private final List<Mapping> mappings;
+
+    public SettlementTypeHelper(List<Mapping> mappings) {
+        this.mappings = mappings;
+    }
+
     public void setSettlementType(Path settlementCurrencySynonymPath, SettlementTerms.SettlementTermsBuilder settlementTermsBuilder) {
-        if (settlementCurrencySynonymPath.endsWith("commoditySwap", "settlementCurrency")
+        if (isCommoditySwap(settlementCurrencySynonymPath)
                 || settlementCurrencySynonymPath.endsWith("exercise", "settlementCurrency")) {
             Optional.ofNullable(settlementTermsBuilder)
                     .filter(this::settlementCurrencyExists)
@@ -20,6 +28,19 @@ public class SettlementTypeHelper {
                         }
                     });
         }
+    }
+
+    private boolean isCommoditySwap(Path settlementCurrencySynonymPath) {
+        return settlementCurrencySynonymPath.endsWith("commoditySwap", "settlementCurrency")
+                // does not have a physical leg
+                && mappings.stream()
+                .map(Mapping::getXmlPath)
+                .map(String::valueOf)
+                .noneMatch(p -> p.contains("commoditySwap.coalPhysicalLeg")
+                        || p.contains("commoditySwap.electricityPhysicalLeg")
+                        || p.contains("commoditySwap.environmentalPhysicalLeg")
+                        || p.contains("commoditySwap.gasPhysicalLeg")
+                        || p.contains("commoditySwap.oilPhysicalLeg"));
     }
 
     private boolean settlementCurrencyExists(SettlementTerms.SettlementTermsBuilder builder) {
