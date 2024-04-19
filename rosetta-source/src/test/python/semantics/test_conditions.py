7e9da8da347333068a94b096066b9fab567d7fe3
dschwartznyc <daniel.schwartz@ftadvisory.co>
2024-04-19 06:21:09 -0400

    Changes to utilize the new Python code generator (v2) (#2869)
    * update to use the new Python Generator (v2).  Includes expanded Python unit tests
    
    * cleanup validation test
    
    * Python Generator Cleanup
    
    * revert bundle usage in pom
    
    * additional pom cleanup
    
    * Updated to align to CDM bundle changes
    
    * additional alignment to next version of CDM
    
    * release.md clean up
    
    * one more release.md update
    
    * Update pom.xml to use correct bundle version
    
    ---------
    
    Co-authored-by: minesh-s-patel <minesh-s-patel@users.noreply.github.com>

--- rosetta-source/src/test/python/semantics/test_conditions.py
+++ rosetta-source/src/test/python/semantics/test_conditions.py
@@ -1,40 +1,50 @@
+'''Full attribute validation - pydantic and constraints'''
 import pytest
 from pydantic import ValidationError
 from rosetta.runtime.utils import ConditionViolationError
 from cdm.base.math.NonNegativeQuantity import NonNegativeQuantity
 from cdm.base.math.UnitType import UnitType
+from cdm.base.datetime.Frequency import Frequency
+from cdm.base.datetime.PeriodExtendedEnum import PeriodExtendedEnum
 
-'''
-def test_recursive_conds():
-    unit = UnitType(currency='EUR')
-    mq = NonNegativeQuantity(value=10, unit=unit)
-    mq.validate_model()
 
-'''
-
-def test_recursive_conds_base_fail():
+def test_recursive_conditions_base_fail():
+    '''condition_0_AmountOnlyExists violation'''
     unit = UnitType(currency='EUR')
     mq = NonNegativeQuantity(unit=unit)
     with pytest.raises(ConditionViolationError):
         mq.validate_model()
 
-def test_recursive_conds_direct_fail():
+
+def test_recursive_conditions_direct_fail():
+    '''Negative quantity condition violation'''
     unit = UnitType(currency='EUR')
     mq = NonNegativeQuantity(value=-10, unit=unit)
     with pytest.raises(ConditionViolationError):
         mq.validate_model()
 
 
-def test_attrib_validity():
+def test_bad_attrib_validation():
+    '''Invalid attribute assigned'''
     unit = UnitType(currency='EUR')
     mq = NonNegativeQuantity(value=10, unit=unit)
     mq.frequency = 'Blah'
     with pytest.raises(ValidationError):
         mq.validate_model()
 
+
+def test_correct_attrib_validation():
+    '''Valid attribute assigned'''
+    unit = UnitType(currency='EUR')
+    mq = NonNegativeQuantity(value=10, unit=unit)
+    mq.frequency = Frequency(periodMultiplier=1, period=PeriodExtendedEnum.M)
+    mq.validate_model()
+
+
 if __name__ == "__main__":
-	test_recursive_conds_base_fail()
-	test_recursive_conds_direct_fail()
-	test_attrib_validity()
+    test_recursive_conditions_base_fail()
+    test_recursive_conditions_direct_fail()
+    test_bad_attrib_validation()
+    test_correct_attrib_validation()
 
 # EOF
