7c31fdd40ca0e59cd7e74d45f1e756fa57cc8a58
Chris <112546065+chrisisla@users.noreply.github.com>
2024-04-19 10:11:40 +0100

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,5 +1,5 @@
 module.exports = {
   mainSidebar: {
-    'Main Menu': ["home", "cdm-overview", "common-domain-model", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping", "namespace", "use-cases", "eligible-collateral-representation", "repurchase-agreement-representation", "securities-lending", "cdm-guidelines", "contribution", "download", "get-involved"]
+    'Main Menu': ["home", "cdm-overview", "common-domain-model", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping", "namespace", "use-cases", "eligible-collateral-representation", "repurchase-agreement-representation", "securities-lending", "pre-trade-processing", "cdm-guidelines", "contribution", "download", "get-involved"]
   },
 }
