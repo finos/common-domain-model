2ee6142272c4af04d74b9a34e7b702e0f53fceb9
eteridvalishvili <117176765+eteridvalishvili@users.noreply.github.com>
2024-04-30 15:27:25 -0400

    Update featuresTwo-config.js

--- website/src/components/featuresTwo-config.js
+++ website/src/components/featuresTwo-config.js
@@ -4,7 +4,7 @@ export const featuresTwo = [
     {
         title: <>ENHANCE INTEROPERABILITY & STRAIGHT THROUGH PROCESSING</>,
         description: (
-            <>Enables interoperability between systems/services, removing burden of setting up connections to different systems/entities, laying groundwork for STP</>
+            <>FINOS CDM enables interoperability between systems/services, removing burden of setting up connections to different systems/entities, laying groundwork for STP</>
         ),
         imageUrl: '/img/feature-images/share.svg',
     },
@@ -12,38 +12,13 @@ export const featuresTwo = [
         title: <>DELIVERS BETTER REGULATORY OVERSIGHT<br/><br/></>,
         imageUrl: '/img/feature-images/book-open.svg',
         description: (
-            <>Promotes transparency and alignment between regulators and market participants</>
+            <>FINOS CDM promotes transparency and alignment between regulators and market participants.</>
         ),
     },
     {
         title: <>CREATES AN ENVIRONMENT FOR INNOVATION IN FINANCIAL MARKETS</>,
         description: (
-            <>Will speed up development of new solutions for the market by allowing providers to focus on what they specialize in rather than requiring them to interpret and represent market events and processes individually</>
-        ),
-        imageUrl: '/img/feature-images/bulb.svg',
-    }
-];
-import React from 'react';
-
-export const featuresTwo = [
-    {
-        title: <>For Derivates</>,
-        description: (
-            <>Streamlines onboarding & enhances interoperability, decreases operational risks & settlement fails, optimizes collateral management & negotiation time</>
-        ),
-        imageUrl: '/img/feature-images/share.svg',
-    },
-    {
-        title: <>For Securities Lending<br/><br/></>,
-        imageUrl: '/img/feature-images/book-open.svg',
-        description: (
-            <>Enables easier construction of smart contracts & faster connection to distributed ledgers and promotes innovation & competition on products rather than infrastructure</>
-        ),
-    },
-    {
-        title: <>For Bonds and Repos</>,
-        description: (
-            <>Improves internal efficiencies by enabling IT applications to communicate effectively & enhances consistency in regulatory reporting & facilitates interoperability between market infrastructures</>
+            <>FINOS CDM will speed up development of new solutions for the market by allowing providers to focus on what they specialize in rather than requiring them to interpret and represent market events and processes individually.</>
         ),
         imageUrl: '/img/feature-images/bulb.svg',
     }
