e15bd7841474b91962cec9f96a79f47edc1f1bf9
eteridvalishvili <117176765+eteridvalishvili@users.noreply.github.com>
2024-04-30 15:14:02 -0400

    Update featuresTwoHeader-config.js

--- website/src/components/featuresTwoHeader-config.js
+++ website/src/components/featuresTwoHeader-config.js
@@ -18,43 +18,44 @@ export const featuresTwoHeader = [
         description: (
             <>
             <br />
-            <span style={{ fontSize: '1.25em' }}><strong>The Common Domain Model (CDM) offers several benefits:</strong></span>
+            <span style={{ fontSize: '1.25em' }}>Development of the Common Domain Model in the financial industry promotes transparency, efficiency, and innovation, and can bring significant benefits to market participants and regulators.</span>
             <br />
             <br />
+            <span style={{ fontSize: '1.25em' }}><strong>Benefits for Derivatives:</strong></span>
             <ul>
-                <li>Streamlines onboarding and enhances interoperability for Derivatives.</li>
-                <li>Decreases operational risks and settlement fails for Derivatives.</li>
-                <li>Optimizes collateral management and negotiation time for Derivatives.</li>
+                <li>Streamlines onboarding and enhances interoperability.</li>
+                <li>Decreases operational risks and settlement fails.</li>
+                <li>Optimizes collateral management and negotiation time.</li>
             </ul>
-            <br />
+            <span style={{ fontSize: '1.25em' }}><strong>Benefits for Securities Lending:</strong></span>
             <ul>
-                <li>Enables easier construction of smart contracts and faster connection to distributed ledgers for Securities Lending.</li>
-                <li>Promotes innovation and competition on products rather than infrastructure for Securities Lending.</li>
+                <li>Enables easier construction of smart contracts and faster connection to distributed ledgers.</li>
+                <li>Promotes innovation and competition on products rather than infrastructure.</li>
             </ul>
-            <br />
+            <span style={{ fontSize: '1.25em' }}><strong>Benefits for Bonds & Repos:</strong></span>
             <ul>
-                <li>Improves internal efficiencies by enabling IT applications to communicate effectively for Bonds & Repos.</li>
-                <li>Enhances consistency in regulatory reporting and facilitates interoperability between market infrastructures for Bonds & Repos.</li>
+                <li>Improves internal efficiencies by enabling IT applications to communicate effectively.</li>
+                <li>Enhances consistency in regulatory reporting and facilitates interoperability between market infrastructures.</li>
             </ul>
             </>
         ),
     },
-    // {
-    //     title: <>WHAT IS THE PURPOSE OF THE CDM?</>,
-    //     description: (
-    //         <>
-    //         <br />
-    //         <span style={{ fontSize: '1.25em' }}><strong>A single, digital processing standard for trade events and actions enhances financial markets' operational efficiency in several ways:</strong></span>
-    //         <br />
-    //         <br />
-    //         <ul>
-    //             <li>Enables interoperability and straight-through processing across firms, solutions, and platforms, reducing the need for reconciliation caused by variations in how each firm records trade lifecycle events.</li>
-    //             <br />
-    //             <li>Accelerates financial technology innovation by providing a common, readily operational foundation for how technologies like distributed ledger, smart contracts, cloud computing, and artificial intelligence can be applied to financial markets.</li>
-    //             <br />
-    //             <li>Delivers better regulatory oversight, promotes transparency and alignment between regulators and market participants, and enables consistency in regulatory reporting by providing a standard representation of trade data and supporting machine-executable reporting requirements.</li>
-    //         </ul>
-    //       </>
-    //     ),
-    // },
+    {
+        title: <>WHAT IS THE PURPOSE OF THE CDM?</>,
+        description: (
+            <>
+            <br />
+            <span style={{ fontSize: '1.25em' }}><strong>A single, digital processing standard for trade events and actions enhances financial markets' operational efficiency in several ways:</strong></span>
+            <br />
+            <br />
+            <ul>
+                <li>Enables interoperability and straight-through processing across firms, solutions, and platforms, reducing the need for reconciliation caused by variations in how each firm records trade lifecycle events.</li>
+                <br />
+                <li>Accelerates financial technology innovation by providing a common, readily operational foundation for how technologies like distributed ledger, smart contracts, cloud computing, and artificial intelligence can be applied to financial markets.</li>
+                <br />
+                <li>Delivers better regulatory oversight, promotes transparency and alignment between regulators and market participants, and enables consistency in regulatory reporting by providing a standard representation of trade data and supporting machine-executable reporting requirements.</li>
+            </ul>
+            </>
+        ),
+    },
 ];
