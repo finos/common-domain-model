f8bc96b861c13c5ad88690bde7fdfe2776ed982c
eteridvalishvili <117176765+eteridvalishvili@users.noreply.github.com>
2024-04-30 15:25:01 -0400

    Update featuresTwo-config.js

--- website/src/components/featuresTwo-config.js
+++ website/src/components/featuresTwo-config.js
@@ -12,13 +12,38 @@ export const featuresTwo = [
         title: <>DELIVERS BETTER REGULATORY OVERSIGHT<br/><br/></>,
         imageUrl: '/img/feature-images/book-open.svg',
         description: (
-            <>Promotes transparency and alignment between regulators and market participants.</>
+            <>Promotes transparency and alignment between regulators and market participants</>
         ),
     },
     {
         title: <>CREATES AN ENVIRONMENT FOR INNOVATION IN FINANCIAL MARKETS</>,
         description: (
-            <>Will speed up development of new solutions for the market by allowing providers to focus on what they specialize in rather than requiring them to interpret and represent market events and processes individually.</>
+            <>Will speed up development of new solutions for the market by allowing providers to focus on what they specialize in rather than requiring them to interpret and represent market events and processes individually</>
+        ),
+        imageUrl: '/img/feature-images/bulb.svg',
+    }
+];
+import React from 'react';
+
+export const featuresTwo = [
+    {
+        title: <>For Derivates</>,
+        description: (
+            <>Streamlines onboarding & enhances interoperability, decreases operational risks & settlement fails, optimizes collateral management & negotiation time</>
+        ),
+        imageUrl: '/img/feature-images/share.svg',
+    },
+    {
+        title: <>For Securities Lending<br/><br/></>,
+        imageUrl: '/img/feature-images/book-open.svg',
+        description: (
+            <>Enables easier construction of smart contracts & faster connection to distributed ledgers and promotes innovation & competition on products rather than infrastructure</>
+        ),
+    },
+    {
+        title: <>For Bonds and Repos</>,
+        description: (
+            <>Improves internal efficiencies by enabling IT applications to communicate effectively & enhances consistency in regulatory reporting & facilitates interoperability between market infrastructures</>
         ),
         imageUrl: '/img/feature-images/bulb.svg',
     }
