a3d99fb7fd18dc4bb1c401b0c3855c08d8eae6d7
eteridvalishvili <117176765+eteridvalishvili@users.noreply.github.com>
2024-04-30 15:49:13 -0400

    Create featuresOne-config.js

---
+++ website/src/components/featuresOne-config.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const featuresTwo = [
+    {
+        title: <>For Derivatives</>,
+        description: (
+            <>Streamlines onboarding & enhances interoperability, decreases operational risks & settlement fails, optimizes collateral management & negotiation time.</>
+        ),
+        imageUrl: '/img/feature-images/share.svg',
+    },
+    {
+        title: <>For Securities Lending<br/><br/></>,
+        imageUrl: '/img/feature-images/book-open.svg',
+        description: (
+            <>Enables easier construction of smart contracts & faster connection to distributed ledgers and promotes innovation & competition on products rather than infrastructure</>
+        ),
+    },
+    {
+        title: <>For Bonds and Repos</>,
+        description: (
+            <>Improves internal efficiencies by enabling IT applications to communicate effectively & enhances consistency in regulatory reporting & facilitates interoperability between market infrastructures</>
+        ),
+        imageUrl: '/img/feature-images/bulb.svg',
+    }
+];
