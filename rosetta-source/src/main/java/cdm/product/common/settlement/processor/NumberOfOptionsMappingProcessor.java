6067adb67ec4380f93275c8590bf09d6df075f69
Hugo Hills <39260692+hugohills-regnosys@users.noreply.github.com>
2024-08-27 11:39:55 +0100

    [5.x.x] Fix address/location references on nested model types (#3100)
    * Cherry pick commit
    
    * Fix commodity classification mapper
    
    * Fix FIS mappings
    
    * Update RELEASE.md

--- rosetta-source/src/main/java/cdm/product/common/settlement/processor/NumberOfOptionsMappingProcessor.java
+++ rosetta-source/src/main/java/cdm/product/common/settlement/processor/NumberOfOptionsMappingProcessor.java
@@ -4,12 +4,9 @@ import cdm.base.math.FinancialUnitEnum;
 import cdm.base.math.NonNegativeQuantitySchedule;
 import cdm.base.math.UnitType;
 import cdm.product.common.settlement.PriceQuantity;
-import com.regnosys.rosetta.common.translation.MappingContext;
-import com.regnosys.rosetta.common.translation.MappingProcessor;
-import com.regnosys.rosetta.common.translation.MappingProcessorUtils;
-import com.regnosys.rosetta.common.translation.Path;
-import com.regnosys.rosetta.common.util.PathUtils;
+import com.regnosys.rosetta.common.translation.*;
 import com.rosetta.model.lib.RosettaModelObjectBuilder;
+import com.rosetta.model.lib.meta.Reference;
 import com.rosetta.model.lib.path.RosettaPath;
 
 import java.math.BigDecimal;
@@ -29,61 +26,80 @@ import static com.regnosys.rosetta.common.util.PathUtils.toPath;
 @SuppressWarnings("unused")
 public class NumberOfOptionsMappingProcessor extends MappingProcessor {
 
-	private static final Path EQUITY_UNDERLIER_PATH = Path.parse("underlyer.singleUnderlyer.equity.instrumentId");
-	private static final Path INDEX_UNDERLIER_PATH = Path.parse("underlyer.singleUnderlyer.index.instrumentId");
+    private static final Path EQUITY_UNDERLIER_PATH = Path.parse("underlyer.singleUnderlyer.equity.instrumentId");
+    private static final Path INDEX_UNDERLIER_PATH = Path.parse("underlyer.singleUnderlyer.index.instrumentId");
 
-	public NumberOfOptionsMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext context) {
-		super(modelPath, synonymPaths, context);
-	}
+    public NumberOfOptionsMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext context) {
+        super(modelPath, synonymPaths, context);
+    }
 
-	@Override
-	public void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent) {
-		NonNegativeQuantitySchedule.NonNegativeQuantityScheduleBuilder quantity = NonNegativeQuantitySchedule.builder();
-		setAmountAndUnit(synonymPath, quantity, builder.size());
-		setMultiplierAndUnit(synonymPath, quantity);
+    @Override
+    public void map(Path synonymPath, List<? extends RosettaModelObjectBuilder> builder, RosettaModelObjectBuilder parent) {
+        NonNegativeQuantitySchedule.NonNegativeQuantityScheduleBuilder quantity = NonNegativeQuantitySchedule.builder();
+        setAmountAndUnit(synonymPath, quantity, builder.size());
+        setMultiplierAndUnit(synonymPath, quantity);
 
-		if (quantity.hasData()) {
-			((PriceQuantity.PriceQuantityBuilder) parent).addQuantity(toReferencableQuantityBuilder(quantity));
-		}
-	}
+        if (quantity.hasData()) {
+            ((PriceQuantity.PriceQuantityBuilder) parent).addQuantity(toReferencableQuantityBuilder(quantity));
+        }
+    }
 
-	private void setAmountAndUnit(Path synonymPath, NonNegativeQuantitySchedule.NonNegativeQuantityScheduleBuilder quantity, int index) {
-		Path baseModelPath = toPath(getModelPath());
-		Path mappedModelPath = incrementPathElementIndex(baseModelPath, "quantity", index);
+    private void setAmountAndUnit(Path synonymPath, NonNegativeQuantitySchedule.NonNegativeQuantityScheduleBuilder quantityBuilder, int index) {
+        Path baseModelPath = toPath(getModelPath());
+        Path mappedModelPath = incrementPathElementIndex(baseModelPath, "quantity", index);
 
-		MappingProcessorUtils.setValueAndUpdateMappings(synonymPath,
-				(xmlValue) -> quantity
-						.setValue(new BigDecimal(xmlValue))
-						.setUnit(UnitType.builder().setFinancialUnit(FinancialUnitEnum.CONTRACT)),
-				getMappings(),
-				PathUtils.toRosettaPath(mappedModelPath));
-	}
+        
 
-	private void setMultiplierAndUnit(Path synonymPath, NonNegativeQuantitySchedule.NonNegativeQuantityScheduleBuilder quantity) {
-		Path parentSynonymPath = synonymPath.getParent();
+        MappingProcessorUtils.getNonNullMappedValue(synonymPath, getMappings()).ifPresent(xmlValue -> {
+            quantityBuilder
+                    .setValue(new BigDecimal(xmlValue))
+                    .setUnit(UnitType.builder().setFinancialUnit(FinancialUnitEnum.CONTRACT));
+            // add new mapping rather than updating, otherwise the referencing breaks
+            addMapping(synonymPath, xmlValue, mappedModelPath, xmlValue);
+            // clean up errors
+            updateReferenceMapping(synonymPath);
+        });
+    }
 
+    private void setMultiplierAndUnit(Path synonymPath, NonNegativeQuantitySchedule.NonNegativeQuantityScheduleBuilder quantity) {
+        Path parentSynonymPath = synonymPath.getParent();
 
-		setValueAndUpdateMappings(parentSynonymPath.addElement("optionEntitlement"),
-				(xmlValue) -> quantity.getOrCreateMultiplier().setValue(new BigDecimal(xmlValue)));
+        setValueAndUpdateMappings(parentSynonymPath.addElement("optionEntitlement"),
+                (xmlValue) -> quantity.getOrCreateMultiplier().setValue(new BigDecimal(xmlValue)));
 
-		// bond option multiplier unit
-		setValueAndUpdateMappings(parentSynonymPath.addElement("entitlementCurrency"),
-				(xmlValue) -> quantity.getOrCreateMultiplier().setUnit(UnitType.builder().setCurrencyValue(xmlValue)));
-		setValueAndUpdateMappings(parentSynonymPath.addElement("entitlementCurrency").addElement("currencyScheme"),
-				(xmlValue) -> quantity.getOrCreateMultiplier().getOrCreateUnit().getOrCreateCurrency().getOrCreateMeta().setScheme(xmlValue));
-		// equity multiplier unit
-		if (pathExists(EQUITY_UNDERLIER_PATH)) {
-			quantity.getOrCreateMultiplier().setUnit(UnitType.builder().setFinancialUnit(FinancialUnitEnum.SHARE));
-		}
-		// index multiplier unit
-		if (pathExists(INDEX_UNDERLIER_PATH)) {
-			quantity.getOrCreateMultiplier().setUnit(UnitType.builder().setFinancialUnit(FinancialUnitEnum.INDEX_UNIT));
-		}
-	}
+        // bond option multiplier unit
+        setValueAndUpdateMappings(parentSynonymPath.addElement("entitlementCurrency"),
+                (xmlValue) -> quantity.getOrCreateMultiplier().setUnit(UnitType.builder().setCurrencyValue(xmlValue)));
+        setValueAndUpdateMappings(parentSynonymPath.addElement("entitlementCurrency").addElement("currencyScheme"),
+                (xmlValue) -> quantity.getOrCreateMultiplier().getOrCreateUnit().getOrCreateCurrency().getOrCreateMeta().setScheme(xmlValue));
+        // equity multiplier unit
+        if (pathExists(EQUITY_UNDERLIER_PATH)) {
+            quantity.getOrCreateMultiplier().setUnit(UnitType.builder().setFinancialUnit(FinancialUnitEnum.SHARE));
+        }
+        // index multiplier unit
+        if (pathExists(INDEX_UNDERLIER_PATH)) {
+            quantity.getOrCreateMultiplier().setUnit(UnitType.builder().setFinancialUnit(FinancialUnitEnum.INDEX_UNIT));
+        }
+    }
 
-	private boolean pathExists(Path endsWith) {
-		return getMappings().stream()
-				.filter(m -> m.getXmlPath().endsWith(endsWith))
-				.anyMatch(m -> m.getXmlValue() != null);
-	}
+    private boolean pathExists(Path endsWith) {
+        return getMappings().stream()
+                .filter(m -> m.getXmlPath().endsWith(endsWith))
+                .anyMatch(m -> m.getXmlValue() != null);
+    }
+
+    private void addMapping(Path xmlPath, Object xmlValue, Path modelPath, Object modelValue) {
+        getMappings().add(new Mapping(xmlPath, xmlValue, modelPath, modelValue, null, true, true, false));
+    }
+
+    private void updateReferenceMapping(Path synonymPath) {
+        getMappings().stream()
+                .filter(m -> m.getXmlPath().equals(synonymPath))
+                .filter(m -> m.getRosettaValue() instanceof Reference.ReferenceBuilder)
+                .filter(m -> m.getError() != null)
+                .forEach(m -> {
+                    m.setDuplicate(false);
+                    m.setError(null);
+                });
+    }
 }
