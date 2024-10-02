eeb852f260cdea764fec91dabb02976e430b64e7
Hugo Hills <hugo.hills@regnosys.com>
2024-10-02 09:33:14 +0000

    formatting

--- rosetta-source/src/main/java/cdm/product/common/settlement/functions/UpdateAmountForEachQuantityImpl.java
+++ rosetta-source/src/main/java/cdm/product/common/settlement/functions/UpdateAmountForEachQuantityImpl.java
@@ -2,14 +2,14 @@ package cdm.product.common.settlement.functions;
 
 import cdm.base.math.NonNegativeQuantitySchedule;
 import cdm.base.math.metafields.FieldWithMetaNonNegativeQuantitySchedule;
-import cdm.product.common.settlement.PriceQuantity;
+import cdm.observable.asset.PriceQuantity;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static cdm.base.math.metafields.FieldWithMetaNonNegativeQuantitySchedule.*;
+import static cdm.base.math.metafields.FieldWithMetaNonNegativeQuantitySchedule.FieldWithMetaNonNegativeQuantityScheduleBuilder;
 import static com.rosetta.util.CollectionUtils.emptyIfNull;
 
 public class UpdateAmountForEachQuantityImpl extends UpdateAmountForEachQuantity {
