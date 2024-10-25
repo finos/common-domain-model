d8087b544ad04a9a3f6ba96c5b39ba45e3129828
Jayasri Radhakrishnan <jayasri.radhakrishnan@regnosys.com>
2024-10-25 18:25:12 +0100

    update expectations

--- rosetta-source/src/main/java/cdm/event/workflow/processor/FISMapperMappingProcessor.java
+++ rosetta-source/src/main/java/cdm/event/workflow/processor/FISMapperMappingProcessor.java
@@ -9,7 +9,6 @@ import cdm.base.math.UnitType;
 import cdm.base.staticdata.asset.common.AssetIdTypeEnum;
 import cdm.base.staticdata.asset.common.AssetIdentifier;
 import cdm.base.staticdata.asset.common.Security;
-import cdm.base.staticdata.asset.common.SecurityTypeEnum;
 import cdm.base.staticdata.party.CounterpartyRoleEnum;
 import cdm.base.staticdata.party.PartyIdentifier;
 import cdm.event.common.ExecutionTypeEnum;
@@ -93,8 +92,6 @@ public class FISMapperMappingProcessor extends FlatFileMappingProcessor<Workflow
         getIRP(tradeState).getValue().getOrCreatePaymentDates().getOrCreatePaymentFrequency().setPeriod(PeriodExtendedEnum.T);
         getIRP(tradeState).getValue().getOrCreatePaymentDates().getOrCreatePaymentFrequency().setPeriodMultiplier(1);
 
-        //sec lending payout
-        getSecPO(tradeState).getValue().getOrCreateUnderlier().getOrCreateInstrument().getOrCreateSecurity().setSecurityType(SecurityTypeEnum.EQUITY);
 
         getSecPO(tradeState).getValue().getOrCreatePayerReceiver().setPayer(CounterpartyRoleEnum.PARTY_1);
         getSecPO(tradeState).getValue().getOrCreatePayerReceiver().setReceiver(CounterpartyRoleEnum.PARTY_2);
@@ -338,7 +335,6 @@ public class FISMapperMappingProcessor extends FlatFileMappingProcessor<Workflow
             // key
             PathValue<PriceQuantityBuilder> pq = getPriceQuantityForSecurityFinancePayout(tradeState);
             Security security = Security.builder()
-                    .setSecurityType(SecurityTypeEnum.EQUITY)
                     .addIdentifier(AssetIdentifier.builder()
                             .setIdentifierValue(value)
                             .setIdentifierType(AssetIdTypeEnum.SEDOL));
