42947661c43148c1fdf1ae21e9f273a46efa1053
eteridvalishvili <117176765+eteridvalishvili@users.noreply.github.com>
2024-10-31 14:50:29 -0400

    Update index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -25,7 +25,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted
               )}
-              to={'docs/training'}>
+              to={'/docs/training'}>
               Introduction Training
            </Link>     
            <Link
