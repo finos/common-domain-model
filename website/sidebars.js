9fb777c08b9284eac476a852fb528a9d4006866d
igallizugaro <igallizugaro@isda.org>
2024-12-16 16:25:58 +0000

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,5 +1,5 @@
 module.exports = {
   mainSidebar: {
-    'Main Menu': ["home", "cdm-overview", "common-domain-model", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping","namespace", "use-cases", "eligible-collateral-representation", "repurchase-agreement-representation", "securities-lending", "pre-trade-processing", "cdm-guidelines", "contribution", "download", "cdm-materials", "get-involved", "CDM-Contribution-Review-WG", "CDM-Collateral-WG", "CDM-Steering-WG", "CDM-Structured-Products-WG", "CDM-Technology-Architecture-WG", "CDM-Derivatives-Product-and-Business-Event-WG"]
+    'Main Menu': ["home", "cdm-overview", "governance", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping","namespace", "use-cases", "eligible-collateral-representation", "repurchase-agreement-representation", "securities-lending", "pre-trade-processing", "java-usage", "contributions", "download", "cdm-materials", "get-involved", "CDM-Contribution-Review-WG", "CDM-Collateral-WG", "CDM-Steering-WG", "CDM-Structured-Products-WG", "CDM-Technology-Architecture-WG", "CDM-Derivatives-Product-and-Business-Event-WG"]
   },
 }
