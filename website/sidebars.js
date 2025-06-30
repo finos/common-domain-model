79a694a0a99c70363efee5c3b6d97d31280c5dda
Eleonora Acuña <eacuna@isda.org>
2025-06-30 12:06:11 -0400

    Update sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,9 +1,6 @@
 module.exports = {
   mainSidebar: {
-<<<<<<< Updated upstream
-    'Main Menu': ["home", "cdm-overview", "governance", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping","namespace", "use-cases", "eligible-collateral-representation", "standardized-functions", "repurchase-agreement-representation", "securities-lending", "pre-trade-processing", "java-usage", "dev-guidelines", "download", "cdm-materials", "cdm-demos", "get-involved", "CDM-Contribution-Review-WG", "CDM-Collateral-WG", "CDM-Steering-WG", "CDM-Structured-Products-WG", "CDM-Technology-Architecture-WG", "CDM-Derivatives-Product-and-Business-Event-WG"]
-=======
-    'Main Menu': ["home", "cdm-overview", "common-domain-model", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping","namespace", "use-cases", "eligible-collateral-representation", "standardized-functions", "repurchase-agreement-representation", "securities-lending", "pre-trade-processing", "cdm-java-distribution", "dev-guidelines", "download", "cdm-materials", "cdm-demos", "get-involved", "CDM-Contribution-Review-WG", "CDM-Collateral-WG", "CDM-Steering-WG", "CDM-Structured-Products-WG", "CDM-Technology-Architecture-WG", "CDM-Derivatives-WG"]
->>>>>>> Stashed changes
+    'Main Menu': ["home", "cdm-overview", "common-domain-model", "product-model", "event-model", "legal-agreements", "process-model", "reference-data-model", "mapping","namespace", "use-cases", "eligible-collateral-representation", "standardized-functions", "repurchase-agreement-representation", "securities-lending", "pre-trade-processing", "cdm-java-distribution", "dev-guidelines", "download", "cdm-materials", "cdm-demos", "get-involved", "CDM-Contribution-Review-WG", "CDM-Collateral-WG", "CDM-Steering-WG",
+     "CDM-Tokenized-Assets-WG", "CDM-Physical-Risk-WG","CDM-Structured-Products-WG", "CDM-Technology-Architecture-WG", "CDM-Derivatives-WG"]
   },
 }
