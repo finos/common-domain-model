ff26c89d45359a0c9c67e79e87ea57e78bc06969
Jayasri Radhakrishnan <jayasri.radhakrishnan@regnosys.com>
2025-07-10 17:55:43 +0100

    attach javadocs and sources for scala

--- rosetta-source/src/main/resources/build-resources/create-deploy-pom.sh
+++ rosetta-source/src/main/resources/build-resources/create-deploy-pom.sh
@@ -121,5 +121,43 @@ cat > pom.xml <<EOF
             </build>
         </profile>
     </profiles>
+EOF
+
+if [ "$ARTIFACT_ID" == "cdm-scala" ]; then
+cat >> pom.xml <<'EOF'
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-source-plugin</artifactId>
+                <version>3.2.1</version>
+                <executions>
+                    <execution>
+                        <id>attach-sources</id>
+                        <goals>
+                            <goal>jar</goal>
+                        </goals>
+                    </execution>
+                </executions>
+            </plugin>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-javadoc-plugin</artifactId>
+                <version>3.3.1</version>
+                <executions>
+                    <execution>
+                        <id>attach-javadocs</id>
+                        <goals>
+                            <goal>jar</goal>
+                        </goals>
+                    </execution>
+                </executions>
+            </plugin>
+        </plugins>
+    </build>
+EOF
+fi
+
+cat >> pom.xml <<EOF
 </project>
 EOF
