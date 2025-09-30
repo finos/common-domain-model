c8983ea47744b9487bd2f9beb916f35b8420ebe9
Hugo Hills <hugo.hills@regnosys.com>
2025-09-30 14:06:05 +0100

    Update expectations

--- rosetta-source/src/main/java/cdm/product/template/processor/FxPayerReceiverMappingProcessor.java
+++ rosetta-source/src/main/java/cdm/product/template/processor/FxPayerReceiverMappingProcessor.java
@@ -1,8 +1,8 @@
 package cdm.product.template.processor;
 
+import cdm.base.staticdata.asset.common.processor.FxMetaHelper;
 import cdm.base.staticdata.party.PayerReceiver;
 import cdm.legaldocumentation.contract.processor.PartyMappingHelper;
-import cdm.observable.asset.QuoteBasisEnum;
 import com.regnosys.rosetta.common.translation.MappingContext;
 import com.regnosys.rosetta.common.translation.MappingProcessor;
 import com.regnosys.rosetta.common.translation.Path;
@@ -10,45 +10,35 @@ import com.rosetta.model.lib.RosettaModelObjectBuilder;
 import com.rosetta.model.lib.path.RosettaPath;
 
 import java.util.List;
-import java.util.Optional;
-
-import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.getNonNullMappedValue;
 
 @SuppressWarnings("unused")
 public class FxPayerReceiverMappingProcessor extends MappingProcessor {
 
+    private final FxMetaHelper fxHelper;
+    
     public FxPayerReceiverMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext context) {
         super(modelPath, synonymPaths, context);
+        this.fxHelper = new FxMetaHelper(context.getMappings());
     }
 
     @Override
     public void map(Path synonymPath, RosettaModelObjectBuilder builder, RosettaModelObjectBuilder parent) {
-        getExchangedCurrencyPath(synonymPath).ifPresent(exchangedCurrencyPath -> {
+        fxHelper.getExchangedCurrencyPath(synonymPath).ifPresent(exchangedCurrencyPath -> {
             PayerReceiver.PayerReceiverBuilder payerReceiverBuilder = (PayerReceiver.PayerReceiverBuilder) builder;
             PartyMappingHelper.getInstance(getContext())
                     .ifPresent(helper -> {
-                                helper.setCounterpartyRoleEnum(getModelPath(),
-                                        exchangedCurrencyPath.addElement("payerPartyReference"), payerReceiverBuilder::setPayer);
-                                helper.setCounterpartyRoleEnum(getModelPath(),
-                                        exchangedCurrencyPath.addElement("receiverPartyReference"), payerReceiverBuilder::setReceiver);
+                                helper.setCounterpartyRoleEnum(
+                                        getModelPath(),
+                                        exchangedCurrencyPath.addElement("payerPartyReference"),
+                                        payerReceiverBuilder::setPayer);
+                                helper.setCounterpartyRoleEnum(
+                                        getModelPath(),
+                                        exchangedCurrencyPath.addElement("receiverPartyReference"),
+                                        payerReceiverBuilder::setReceiver);
                             }
                     );
         });
     }
 
-    private Optional<Path> getExchangedCurrencyPath(Path synonymPath) {
-        return getNonNullMappedValue(synonymPath, getMappings())
-                .map(quoteBasis -> {
-                    Path productPath = synonymPath.getParent().getParent().getParent();
-                    QuoteBasisEnum quoteBasisEnum = QuoteBasisEnum.fromDisplayName(quoteBasis);
-                    switch (quoteBasisEnum) {
-                        case CURRENCY_1_PER_CURRENCY_2:
-                            return productPath.addElement("exchangedCurrency2");
-                        case CURRENCY_2_PER_CURRENCY_1:
-                            return productPath.addElement("exchangedCurrency1");
-                        default:
-                            return null;
-                    }
-                });
-    }
+
 }
