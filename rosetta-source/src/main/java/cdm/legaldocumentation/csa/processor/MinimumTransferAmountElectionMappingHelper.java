1b9f519a4c22ac662582177735fdb638cd43e8fc
Jayasri Radhakrishnan <jayasri.radhakrishnan@regnosys.com>
2025-10-24 17:05:04 +0100

    map other in ThresholdElectionMappingHelper and code refactor

--- rosetta-source/src/main/java/cdm/legaldocumentation/csa/processor/MinimumTransferAmountElectionMappingHelper.java
+++ rosetta-source/src/main/java/cdm/legaldocumentation/csa/processor/MinimumTransferAmountElectionMappingHelper.java
@@ -1,6 +1,5 @@
 package cdm.legaldocumentation.csa.processor;
 
-import cdm.base.staticdata.asset.common.ISOCurrencyCodeEnum;
 import cdm.legaldocumentation.csa.MinimumTransferAmountElection;
 import cdm.legaldocumentation.csa.ThresholdMinimumTransferAmountFixedAmount;
 import cdm.observable.asset.Money;
@@ -9,11 +8,9 @@ import com.regnosys.rosetta.common.translation.Path;
 import com.regnosys.rosetta.common.translation.SynonymToEnumMap;
 import com.rosetta.model.lib.path.RosettaPath;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.setValueAndOptionallyUpdateMappings;
 import static com.regnosys.rosetta.common.translation.MappingProcessorUtils.setValueAndUpdateMappings;
 import static org.isda.cdm.processor.CreateiQMappingProcessorUtils.*;
 
@@ -21,26 +18,17 @@ public class MinimumTransferAmountElectionMappingHelper {
 
     private final RosettaPath path;
     private final List<Mapping> mappings;
-    private final SynonymToEnumMap synonymToEnumMap;
+    private final CreateiQMappingHelperUtils createiQMappingHelperUtils;
 
     public MinimumTransferAmountElectionMappingHelper(RosettaPath path, List<Mapping> mappings, SynonymToEnumMap synonymToEnumMap) {
         this.path = path;
         this.mappings = mappings;
-        this.synonymToEnumMap = synonymToEnumMap;
+        this.createiQMappingHelperUtils = new CreateiQMappingHelperUtils(path, mappings, synonymToEnumMap);
     }
 
     public Optional<MinimumTransferAmountElection> getMinimumTransferAmountElection(Path synonymPath, String party) {
         MinimumTransferAmountElection.MinimumTransferAmountElectionBuilder minimumTransferAmountElectionBuilder = MinimumTransferAmountElection.builder();
-        Money.MoneyBuilder moneyBuilder = Money.builder();
-
-        setValueAndUpdateMappings(synonymPath.addElement(party + "_amount"),
-                (value) -> moneyBuilder.setValue(new BigDecimal(value)), mappings, path);
-
-        setValueAndOptionallyUpdateMappings(synonymPath.addElement(party + "_currency"),
-                (value) -> setIsoCurrency(synonymToEnumMap.getEnumValue(ISOCurrencyCodeEnum.class, value),
-                        cur -> moneyBuilder.getOrCreateUnit().setCurrency(cur)),
-                mappings,
-                path);
+        Money.MoneyBuilder moneyBuilder = createiQMappingHelperUtils.getMoneyBuilder(synonymPath, party);
 
         if (moneyBuilder.hasData()) {
             minimumTransferAmountElectionBuilder.setFixedAmount(ThresholdMinimumTransferAmountFixedAmount.builder().setAmount(moneyBuilder));
