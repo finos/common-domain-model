0ce6c40f331f53b033a08fee45d51a31c83f0676
Hugo Hills <hugo.hills@regnosys.com>
2025-11-05 17:25:28 +0000

    Refactor ingestion tests to use `@TestInstance` annotation, and implement `BasketPercentageMappingProcessor` to avoid non-deterministic behaviour with condition mapping. Update surefire memory settings in `pom.xml`.

--- tests/src/test/java/com/regnosys/ingest/fpml/NativeCdmEventsIngestionServiceTest.java
+++ tests/src/test/java/com/regnosys/ingest/fpml/NativeCdmEventsIngestionServiceTest.java
@@ -8,6 +8,7 @@ import com.regnosys.ingest.test.framework.ingestor.IngestionTestUtil;
 import com.regnosys.ingest.test.framework.ingestor.service.IngestionService;
 import org.finos.cdm.CdmRuntimeModule;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.net.URL;
@@ -15,35 +16,36 @@ import java.util.stream.Stream;
 
 import static com.regnosys.ingest.IngestionEnvUtil.getFpml5ConfirmationToWorkflowStep;
 
-public class NativeCdmEventsIngestionServiceTest extends IngestionTest<WorkflowStep> {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class NativeCdmEventsIngestionServiceTest extends IngestionTest<WorkflowStep> {
 
-	private static final String BASE_DIR = "cdm-sample-files/native-cdm-events/";
+    private static final String BASE_DIR = "cdm-sample-files/native-cdm-events/";
 
-	private static ImmutableList<URL> EXPECTATION_FILES = ImmutableList.<URL>builder()
-		.add(Resources.getResource(BASE_DIR + "expectations.json"))
-		.build();
+    private static final ImmutableList<URL> EXPECTATION_FILES = ImmutableList.<URL>builder()
+            .add(Resources.getResource(BASE_DIR + "expectations.json"))
+            .build();
 
-	private static IngestionService ingestionService;
+    private static IngestionService ingestionService;
 
-	@BeforeAll
-	static void setup() {
-		CdmRuntimeModule runtimeModule = new CdmRuntimeModule();
-		initialiseIngestionFactory(runtimeModule, IngestionTestUtil.getPostProcessors(runtimeModule));
-		ingestionService = getFpml5ConfirmationToWorkflowStep();
-	}
+    @BeforeAll
+    static void setup() {
+        CdmRuntimeModule runtimeModule = new CdmRuntimeModule();
+        initialiseIngestionFactory(runtimeModule, IngestionTestUtil.getPostProcessors(runtimeModule));
+        ingestionService = getFpml5ConfirmationToWorkflowStep();
+    }
 
-	@Override
-	protected Class<WorkflowStep> getClazz() {
-		return WorkflowStep.class;
-	}
+    @Override
+    protected Class<WorkflowStep> getClazz() {
+        return WorkflowStep.class;
+    }
 
-	@Override
-	protected IngestionService ingestionService() {
-		return ingestionService;
-	}
+    @Override
+    protected IngestionService ingestionService() {
+        return ingestionService;
+    }
 
-	@SuppressWarnings("unused")//used by the junit parameterized test
-	private static Stream<Arguments> fpMLFiles() {
-		return readExpectationsFrom(EXPECTATION_FILES);
-	}
+    @SuppressWarnings("unused")//used by the junit parameterized test
+    private static Stream<Arguments> fpMLFiles() {
+        return readExpectationsFrom(EXPECTATION_FILES);
+    }
 }
