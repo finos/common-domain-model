63650073564a2f2934fd4754c6d8c4050f051f80
Payal Khanna <107400406+PayalKhanna@users.noreply.github.com>
2025-11-19 19:20:56 +0000

    [7dev] Oscar Twomey Legal Agreement Model - Updating PartyContactInformation in Legal Agreement (#4020)
    * Legal Agreement Model - Updating PartyContactInformation in Legal Agreement
    
    * Updating synonym mappings
    
    * format
    
    * fix mappers
    
    * update expectations
    
    * fix mappers
    
    * update expectations
    
    * Updating types for AddressesForTransfer
    
    * Updating process agent
    
    * Ordering descriptions
    
    * Updating cardinality naming of attributes
    
    * Updating cardinality of elections
    
    * format add Release notes
    
    * update expectations
    
    * update expectations
    
    * update expectations
    
    * Additional information in process agent election
    
    * update expectations and update mapper
    
    * Removing additional information from account
    
    * update mapper
    
    * Update RELEASE.md
    
    * Updating descriptions on the types
    
    * Update PR link format in RELEASE.md
    
    * remove duplicate imports
    
    * Update RELEASE.md
    
    * Update RELEASE.md
    
    * Update RELEASE.md
    
    * format
    
    * update expectations
    
    * add removeHtml to additionalInformation
    
    * Update RELEASE.md
    
    * update expectations
    
    * fix typo
    
    * format update Release notes
    
    * Add config for rosetta-config.yml in pom
    
    * format
    
    * update Bundle version
    
    * fix fis Ingest test
    
    ---------
    
    Co-authored-by: Oscar Twomey <oscar.twomey@regnosys.com>
    Co-authored-by: Jayasri Radhakrishnan <jayasri.radhakrishnan@regnosys.com>
    Co-authored-by: JayasriR <124573358+JayasriR@users.noreply.github.com>
    Co-authored-by: eacunaISDA <82891014+eacunaISDA@users.noreply.github.com>
    Co-authored-by: Hugo Hills <hugo.hills@regnosys.com>

--- rosetta-source/src/main/java/cdm/legaldocumentation/csa/processor/DemandsAndNoticesMappingProcessor.java
+++ rosetta-source/src/main/java/cdm/legaldocumentation/csa/processor/DemandsAndNoticesMappingProcessor.java
@@ -1,9 +1,5 @@
 package cdm.legaldocumentation.csa.processor;
 
-import cdm.base.staticdata.party.Address;
-import cdm.base.staticdata.party.ContactInformation;
-import cdm.base.staticdata.party.PartyContactInformation;
-import cdm.base.staticdata.party.metafields.ReferenceWithMetaParty;
 import cdm.legaldocumentation.csa.DemandsAndNotices;
 import com.regnosys.rosetta.common.translation.MappingContext;
 import com.regnosys.rosetta.common.translation.MappingProcessor;
@@ -13,9 +9,7 @@ import com.rosetta.model.lib.path.RosettaPath;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import static org.isda.cdm.processor.CdmMappingProcessorUtils.removeHtml;
 import static org.isda.cdm.processor.CreateiQMappingProcessorUtils.PARTIES;
 
 /**
@@ -23,8 +17,11 @@ import static org.isda.cdm.processor.CreateiQMappingProcessorUtils.PARTIES;
  */
 @SuppressWarnings("unused")
 public class DemandsAndNoticesMappingProcessor extends MappingProcessor {
+    private final NoticeInformationElectionMappingHelper helper;
+
     public DemandsAndNoticesMappingProcessor(RosettaPath modelPath, List<Path> synonymPaths, MappingContext mappingContext) {
         super(modelPath, synonymPaths, mappingContext);
+        this.helper = new NoticeInformationElectionMappingHelper(getModelPath(), getMappings());
     }
 
     @Override
@@ -32,17 +29,7 @@ public class DemandsAndNoticesMappingProcessor extends MappingProcessor {
         DemandsAndNotices.DemandsAndNoticesBuilder demandsAndNoticesBuilder = (DemandsAndNotices.DemandsAndNoticesBuilder) builder;
         demandsAndNoticesBuilder.setPartyElection(new ArrayList<>())
                 .setDeemedEffectiveNextLBD(false);
-        PARTIES.forEach(party -> getPartyContactInformation(synonymPath, party).ifPresent(demandsAndNoticesBuilder::addPartyElection));
+        PARTIES.forEach(party -> helper.getNoticeInformationElection(synonymPath, party).ifPresent(demandsAndNoticesBuilder::addPartyElection));
     }
 
-    private Optional<PartyContactInformation> getPartyContactInformation(Path synonymPath, String party) {
-        PartyContactInformation.PartyContactInformationBuilder partyContactInformationBuilder = PartyContactInformation.builder();
-        setValueAndUpdateMappings(synonymPath.addElement(party + "_specify"),
-                address -> partyContactInformationBuilder
-                        .setPartyReference(ReferenceWithMetaParty.builder().setExternalReference(party).build())
-                        .setContactInformation(ContactInformation.builder()
-                                .addAddress(Address.builder()
-                                        .addStreet(removeHtml(address)))));
-        return partyContactInformationBuilder.hasData() ? Optional.of(partyContactInformationBuilder.build()) : Optional.empty();
-    }
 }
