54d186f5a4b7e5e67666dfb2aed9c0e8c6d768ae
Hugo Hills <hugo.hills@regnosys.com>
2026-06-01 09:59:33 +0100

    Remove legacy ISO20022 namespace and fix legacy org.isda namespaces

--- rosetta-source/src/test/java/cdm/product/asset/floatingrate/functions/GetRateScheduleAmountTest.java
+++ rosetta-source/src/test/java/cdm/product/asset/floatingrate/functions/GetRateScheduleAmountTest.java
@@ -8,7 +8,7 @@ import cdm.observable.asset.PriceTypeEnum;
 import cdm.product.common.schedule.RateSchedule;
 import javax.inject.Inject;
 import com.rosetta.model.lib.records.Date;
-import org.isda.cdm.functions.AbstractFunctionTest;
+import org.finos.cdm.functions.AbstractFunctionTest;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
