095baa5336ba5cf55fb152465a4276ccc1f55681
Payal Khanna <107400406+PayalKhanna@users.noreply.github.com>
2026-08-18 15:27:40 +0100

    [6xx] update rune-fpml/DSL/bundle dependency (#5051)
    * update rune-fpml/DSL/bundle dependency
    
    * rename rosetta-config.yml to rune-config.yml
    
    * fix test
    
    * revert rune fpml version to
    
    * revert logback and update rune fpml to latest
    
    * added normalizeLineEndings method to handle line endings in test assertions
    
    * using LineEndings Util to normalize JSON output
    
    * using LineEndings Util to normalize JSON output
    
    ---------
    
    Co-authored-by: ReleaseManagement-BP <benjamin.page+cdm@regnosys.com>

--- tests/src/test/java/org/finos/cdm/functions/FunctionInputCreationTest.java
+++ tests/src/test/java/org/finos/cdm/functions/FunctionInputCreationTest.java
@@ -7,6 +7,7 @@ import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.util.Modules;
 import com.regnosys.rosetta.common.postprocess.WorkflowPostProcessor;
+import com.regnosys.rosetta.common.util.LineEndings;
 import com.rosetta.model.lib.process.PostProcessor;
 import jakarta.inject.Inject;
 import org.finos.cdm.CdmRuntimeModule;
@@ -341,8 +342,8 @@ class FunctionInputCreationTest {
     }
 
     private static void assertResult(ExpectationResult<?> result) throws IOException {
-        String expectedJson = ResourcesUtils.getJson(result.getResourceName());
-        String actualJson = OBJECT_WRITER.writeValueAsString(result.getActual());
+        String expectedJson = LineEndings.normalise(ResourcesUtils.getJson(result.getResourceName()));
+        String actualJson = LineEndings.normalise(OBJECT_WRITER.writeValueAsString(result.getActual()));
         assertEquals(expectedJson, actualJson);
     }
 }
